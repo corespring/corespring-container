8afd67a5f0c94c2067ae33599bb7dfcf728fb488
edeustace <ed.eustace@gmail.com>
2013-10-03 20:21:42 +0200

    udpate root editor controller to load the item data

--- modules/player-client/src/main/resources/container-client/js/editor/controllers/root.js
+++ modules/player-client/src/main/resources/container-client/js/editor/controllers/root.js
@@ -1,4 +1,31 @@
 var controller = function ($scope, $compile, $http, $timeout, EditorServices, CorespringContainer) {
-}
+
+  $scope.save = function () {
+    //TODO: updateItem - get the update ready
+    EditorServices.save(updatedItem, $scope.onItemSaved, $scope.onItemSaveError);
+  };
+
+  $scope.onItemSaved = function (data) {
+  };
+
+  $scope.onItemSaveError = function (error) {
+    console.warn("Error saving item");
+  };
+
+  $scope.onItemLoadError = function (error) {
+    console.warn("Error loading item");
+  };
+
+  $scope.onSessionSaveError = function (error) {
+    console.warn("Error saving session");
+  };
+
+  $scope.onItemLoaded = function (data) {
+    $scope.model = data;
+    CorespringContainer.initialize(data);
+  };
+
+  EditorServices.load($scope.onItemLoaded, $scope.onItemLoadError);
+};
 
 angular.module('corespring-editor.controllers').controller('Root', ['$scope', '$compile', '$http', '$timeout', 'EditorServices', 'CorespringContainer', controller]);
