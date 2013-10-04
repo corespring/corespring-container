43fd98dba57f57a8f3682ec212c5eab9ddee6216
edeustace <ed.eustace@gmail.com>
2013-10-04 11:37:02 +0200

    wrap item in 'item' -> ....

--- modules/container-client-web/app/org/corespring/container/controllers/Item.scala
+++ modules/container-client-web/app/org/corespring/container/controllers/Item.scala
@@ -2,13 +2,14 @@ package org.corespring.container.controllers
 
 import play.api.mvc.{AnyContent, Controller}
 import org.corespring.container.player.actions.{SaveItemRequest, ItemRequest, ItemActionBuilder}
+import play.api.libs.json.Json
 
 trait Item extends Controller {
 
   def builder : ItemActionBuilder[AnyContent]
 
   def load(itemId:String) = builder.load(itemId) { request : ItemRequest[AnyContent] =>
-    Ok(request.item)
+    Ok(Json.obj("item" -> request.item))
   }
 
   def save(itemId:String) = builder.save(itemId){ request : SaveItemRequest[AnyContent] =>
