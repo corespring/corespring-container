8413eec129f6f53e39e4a86854df1961f1e7fa8b
Attila Miklosi <attila.miklosi@gmail.com>
2013-10-07 12:36:18 +0200

    added general purpose ng-bind-html-unsafe removed ckeditor directive from container

---
+++ modules/container-client/src/main/resources/container-client/js/editor/directives/ng-bind-html-unsafe.js
@@ -0,0 +1,17 @@
+angular.module('corespring-editor.directives').directive('ngBindHtmlUnsafe', ['$sce', function($sce){
+  return {
+    scope: {
+      ngBindHtmlUnsafe: '=',
+    },
+    template: "<div ng-bind-html='trustedHtml'></div>",
+    link: function($scope, iElm, iAttrs, controller) {
+      $scope.updateView = function() {
+        $scope.trustedHtml = $sce.trustAsHtml($scope.ngBindHtmlUnsafe);
+      }
+
+      $scope.$watch('ngBindHtmlUnsafe', function(newVal, oldVal) {
+        $scope.updateView(newVal);
+      });
+    }
+  };
+}]);
