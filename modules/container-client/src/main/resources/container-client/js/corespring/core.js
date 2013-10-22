a1230d82c05a8120ab0022c497cff4464928db43
edeustace <ed.eustace@gmail.com>
2013-10-22 13:42:37 +0200

    update submodule

--- modules/container-client/src/main/resources/container-client/js/corespring/core.js
+++ modules/container-client/src/main/resources/container-client/js/corespring/core.js
@@ -1,11 +1,23 @@
+/*
+exports.process = function(x,y,z){
+
+}
+
+//Wrapped like so:
+(function(exports, require){
+
+})(corespring.server.logic(compType), corespring.require)
+*/
+
 (function(root){
 
   var Server = function(){
 
     var logic = {};
 
-    this.registerLogic = function(componentType, logic){
-      logic[componentType] = logic;
+    this.logic = function(componentType){
+      logic[componentType] = {};
+      return logic[componentType];
     }
 
   };
