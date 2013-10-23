bca1667b8633286dbf232691a2adb0ccde965985
edeustace <ed.eustace@gmail.com>
2013-10-23 10:48:06 +0200

    get player working

--- modules/container-client/src/main/resources/container-client/js/render/controllers/root.js
+++ modules/container-client/src/main/resources/container-client/js/render/controllers/root.js
@@ -1,22 +1,15 @@
-var controller = function ($scope, $compile, $http, $timeout, PlayerServices, CorespringContainer) {
+var controller = function ($scope, $compile, $http, $timeout, PlayerServices, ComponentRegister) {
+
   $scope.submit = function () {
     PlayerServices.submitAnswers({
-      answers: CorespringContainer.getAnswers()
+      answers: ComponentRegister.getAnswers()
     }, $scope.onSessionSaved, $scope.onSessionSaveError);
   };
 
   $scope.onSessionSaved = function (data) {
-
-    if (data.responses !== null) {
-      CorespringContainer.updateResponses(data.responses);
-    }
-
-    if (data.session !== null) {
-      CorespringContainer.updateSession(data.session);
-    }
-
     $scope.session = data.session;
     $scope.outcome = data.outcome;
+    $scope.responses = data.responses;
   };
 
   $scope.updateSession = function (data) {
@@ -37,20 +30,25 @@ var controller = function ($scope, $compile, $http, $timeout, PlayerServices, Co
   };
 
   $scope.onSessionLoaded = function (data) {
-    $scope.model = data;
-    CorespringContainer.initialize(data);
-    if (data.responses !== null) {
-      CorespringContainer.updateResponses(data.responses);
-    }
-    if (data.session !== null) {
-      CorespringContainer.updateSession(data.session);
-    }
-    if (data.session !== null) {
-      $scope.session = data.session;
-    }
+    $scope.model = data.item;
+    $scope.session = data.session;
+    $scope.outcome = data.outcome;
+    $scope.responses = data.responses;
   };
+
   PlayerServices.loadSession($scope.onSessionLoaded, $scope.onSessionLoadError);
 };
 
 
-angular.module('corespring-player.controllers').controller('Root', ['$scope', '$compile', '$http', '$timeout', 'PlayerServices', 'CorespringContainer', controller]);
+angular.module('corespring-player.controllers')
+  .controller(
+    'Root',
+    [
+    '$scope',
+    '$compile',
+    '$http',
+    '$timeout',
+    'PlayerServices',
+    'ComponentRegister',
+     controller
+    ]);
