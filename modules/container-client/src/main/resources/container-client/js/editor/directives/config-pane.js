6e81b398c8f3e920c70fd9c512c15235858bccf6
Attila Miklosi <attila.miklosi@gmail.com>
2013-11-20 14:30:57 +0100

    added ok button to landing place config popover and made clicking outside close it as well

--- modules/container-client/src/main/resources/container-client/js/editor/directives/config-pane.js
+++ modules/container-client/src/main/resources/container-client/js/editor/directives/config-pane.js
@@ -1,52 +1,58 @@
-(function() {
+(function () {
 
-var link;
+  var link;
 
-link = function ($compile) {
-  return function ($scope, $elem, attrs) {
-    console.log("config pane....");
-    $scope.$watch(attrs.ngModel, function (data) {
-      var $div, tmpl;
-      if (!data) {
-        return;
-      }
-      var configName = data.component.componentType + '-config';
-      console.log("[config pane] model loaded.");
-      tmpl = ['<div class="config-chrome">',
-              '<div class="holder">',
-              '<input type="text" class="title-input" placeholder="Unnamed Component" ng-model="title"></input>',
-              '<' + configName + ' id="' + data.id + '"></' + configName + '>',
-              //'<span class="btn btn-primary btn-xs" ng-click="save()">Save</span>',
-              '</div>',
-              '</div>'].join("\n");
+  link = function ($compile) {
+    return function ($scope, $elem, attrs) {
+      console.log("config pane....");
+      $scope.$watch(attrs.ngModel, function (data) {
+        var $div, tmpl;
+        if (!data) {
+          return;
+        }
+        var configName = data.component.componentType + '-config';
+        console.log("[config pane] model loaded.");
+        tmpl = ['<div class="config-chrome" ng-click="clickOutside($event)">',
+          '<div class="holder">',
+          '<input type="text" class="title-input" placeholder="Unnamed Component" ng-model="title"></input>',
+          '<' + configName + ' id="' + data.id + '"></' + configName + '>',
+          //'<span class="btn btn-primary btn-xs" ng-click="save()">Save</span>',
+          '</div>',
+          '</div>'].join("\n");
 
-      $div = $(tmpl);
-      $elem.html($div);
+        $div = $(tmpl);
+        $elem.html($div);
 
-      var newScope = $scope.$new();
+        var newScope = $scope.$new();
 
-      newScope.title = data.component.title;
-      newScope.$watch('title', function(newTitle) {
-        data.component.title = newTitle;
+        newScope.title = data.component.title;
+        newScope.$watch('title', function (newTitle) {
+          data.component.title = newTitle;
+        });
+        newScope.clickOutside = function (e) {
+          var inPopover = $(e.target).closest('.popover').length > 0
+          if (!inPopover) {
+            newScope.$broadcast('clickOutsidePopover');
+          }
+        };
+
+        return $compile($div)(newScope);
+
+
       });
-
-      return $compile($div)(newScope);
-
-
-    });
-  };
-};
-
-angular.module('corespring-editor.directives').directive('configPane', [
-  '$compile', function ($compile) {
-    var def;
-    def = {
-      link: link($compile),
-      restrict: 'E',
-      template: "<h1>Config pane..</h1>"
     };
-    return def;
-  }
-]);
+  };
+
+  angular.module('corespring-editor.directives').directive('configPane', [
+    '$compile', function ($compile) {
+      var def;
+      def = {
+        link: link($compile),
+        restrict: 'E',
+        template: "<h1>Config pane..</h1>"
+      };
+      return def;
+    }
+  ]);
 
 }).call(this);
