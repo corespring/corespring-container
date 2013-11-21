97d50eb9911266f9f642f95cde94b88a20eaed26
edeustace <ed.eustace@gmail.com>
2013-11-21 11:56:33 +0000

    stub player

--- modules/container-client/src/main/resources/container-client/js/corespring/external-player.js
+++ modules/container-client/src/main/resources/container-client/js/corespring/external-player.js
@@ -1,1 +1,13 @@
 console.log("external player");
+
+(function(root){
+
+  this.org = this.org || {};
+  org.corespring = org.corespring || {};
+  org.corespring.players = org.corespring.players || {};
+
+  org.corespring.players.ItemPlayer = function(opts){
+
+  };
+
+})(this);
--- third-party-simulator/assets/js/app.js
+++ third-party-simulator/assets/js/app.js
@@ -8,7 +8,16 @@ angular.module("simulator").controller('Root', ['$scope', '$log', function($scop
   $scope.mode = "gather";
 
   $scope.idLabel = function(){
-    return $scope.mode == "gather" ? "Item Id" : "Session Id";   
+    return $scope.mode === "gather" ? "Item Id" : "Session Id";   
+  };
+
+  $scope.add = function() { 
+    var options = {};
+    $scope.player = new org.corespring.players.ItemPlayer('#player', options);
+  };
+
+  $scope.remove = function() {
+
   };
 
 }]);
