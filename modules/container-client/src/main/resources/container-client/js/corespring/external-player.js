483cbb44d176aac6ecdc9770aedffebed3dc39ea
Attila Miklosi <attila.miklosi@gmail.com>
2013-11-21 15:22:32 +0100

    external interface of the new version of player wip

--- modules/container-client/src/main/resources/container-client/js/corespring/external-player.js
+++ modules/container-client/src/main/resources/container-client/js/corespring/external-player.js
@@ -1,13 +1,153 @@
 console.log("external player");
 
-(function(root){
+(function (root) {
 
   this.org = this.org || {};
   org.corespring = org.corespring || {};
   org.corespring.players = org.corespring.players || {};
 
-  org.corespring.players.ItemPlayer = function(opts){
+  var rootListener = null;
+  var eventName = function() { return window.addEventListener ? "message" : "onmessage";}
+  var addEventFunctionName = function(){ return window.addEventListener ? "addEventListener" : "attachEvent"; }
 
+  /** A cache of existing player listeners - gets overrwritten when a new ItemPlayer is instantiated */
+  var playerListeners = [];
+
+  var logError = function(error) {
+    console.error(error);
+  };
+
+  var addPlayerListener = function (fn) {
+    if (playerListeners.indexOf(fn) == -1) {
+      playerListeners.push(fn);
+    }
+  };
+
+  var clearPlayerListeners = function () {
+    playerListeners = [];
+  };
+
+  /** only add one listener to the window, this function is expected to then delegate out to player listeners */
+  var addRootLevelMessageListener = function (newListener) {
+    if (rootListener != null) {
+      throw "A root level listener is already registered!";
+    }
+    rootListener = newListener;
+    window[addEventFunctionName()](eventName(), rootListener, false);
+  };
+
+  /** The root listener implementation - forward event to all player listeners */
+  var rootLevelListener = function (e) {
+    for (var i = 0; i < playerListeners.length; i++) {
+      playerListeners[i](e);
+    }
+  };
+
+  addRootLevelMessageListener(rootLevelListener);
+
+  var isValidMode = function (m) {
+    if (!m) return false;
+
+    return ["preview", "administer", "render", "aggregate"].indexOf(m) !== -1;
+  };
+
+  var renderPlayer = function (e, options) {
+    var url = options.corespringUrl;
+    if (options.omitSubmitButton)
+      url += "?omitSubmitButton=true";
+
+    e.html("<iframe id='iframe-player' src='" + url + "' style='width: 100%; height: 100%; border: none'></iframe>");
+    e.width(options.width ? options.width : "600px");
+
+  };
+
+  var expectResult = function(message, callback, dataProcessor) {
+    dataProcessor = dataProcessor || (function(data) {
+      return data;
+    });
+    addPlayerListener(function(event) {
+      try {
+        var dataString = event.data;
+        var data = JSON.parse(dataString);
+        if (data.message == message) {
+          callback(dataProcessor(data));
+        }
+      }
+      catch (e) {
+        logError("Exception in ItemPlayer.addSessionListener: "+e);
+      }
+
+    });
+  };
+
+  org.corespring.players.ItemPlayer = function (elementSelector, options) {
+
+    var postMessage = function(message, data) {
+      console.debug("Posting Message: ",message, data);
+      try {
+        var iframe = $(elementSelector).find('iframe')[0];
+        if (!iframe) throw "iframe not found";
+
+        var messageObject = {"message": message};
+        iframe.contentWindow.postMessage(JSON.stringify($.extend(messageObject, data)), "*");
+        return true;
+      } catch (e) {
+        console.error("no iframe");
+        return false;
+      }
+    };
+
+    var asyncRequest = function(argObj) {
+      postMessage(argObj.message, argObj.data);
+      var extractPropertyFromMessage = function(message) {
+        return message[argObj.property];
+      };
+      expectResult(argObj.message+"Result", argObj.callback, extractPropertyFromMessage);
+    };
+
+    /* API methods */
+    this.setMode = function (mode) {
+      postMessage("setMode", {mode : mode});
+    };
+
+    this.saveResponses = function (isAttempt) {
+      postMessage("saveResponses", {isAttempt : isAttempt});
+    };
+
+    this.completeResponse = function() {
+      postMessage("completeResponse");
+    };
+
+    this.resetItem = function() {
+      postMessage("resetItem");
+    };
+
+    this.countAttempts = function(callback) {
+      asyncRequest({
+        message: "countAttempts",
+        property: "count",
+        callback: callback
+      });
+    };
+
+    this.getScore = function(format, callback) {
+      asyncRequest({
+        message: "getScore",
+        data: {format: format},
+        property: "score",
+        callback: callback
+      });
+    };
+
+    this.isComplete = function(callback) {
+      asyncRequest("isComplete", "isComplete", callback);
+    };
+
+    this.getSessionStatus = function(callback) {
+      asyncRequest("getSessionStatus", "sessionStatus", callback);
+    };
+
+    renderPlayer($(elementSelector), options);
   };
 
 })(this);
