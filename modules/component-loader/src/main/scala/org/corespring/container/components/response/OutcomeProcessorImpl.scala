c7777722391cf4b7428020ee3b6e71bec81e0c32
Attila Miklosi <attila.miklosi@gmail.com>
2013-11-29 14:22:11 +0100

    small refactor of outcome processing

--- modules/component-loader/src/main/scala/org/corespring/container/components/response/OutcomeProcessorImpl.scala
+++ modules/component-loader/src/main/scala/org/corespring/container/components/response/OutcomeProcessorImpl.scala
@@ -4,20 +4,20 @@ import org.corespring.container.components.model.{Library, UiComponent}
 import org.slf4j.LoggerFactory
 import play.api.libs.json.{JsObject, JsValue}
 
-class OutcomeProcessorImpl(components: Seq[UiComponent], libraries : Seq[Library]) extends OutcomeProcessor {
+class OutcomeProcessorImpl(components: Seq[UiComponent], libraries: Seq[Library]) extends OutcomeProcessor {
 
   private lazy val logger = LoggerFactory.getLogger("components.outcome")
 
   def createOutcome(item: JsValue, answers: JsValue, settings: JsValue): JsValue = {
 
-    def getTargetIdFor(id:String) = {
+    def getTargetIdFor(id: String) = {
       val componentQuestions = (item \ "components").as[JsObject]
       val question = (componentQuestions \ id).as[JsObject]
       val targetId = (question \ "target" \ "id").asOpt[String]
       targetId
     }
 
-    def createOutcomeForComponent(id:String, targetOutcome:JsValue):(String, JsValue) = {
+    def createOutcomeForComponent(id: String, targetOutcome: JsValue): (String, JsValue) = {
       val componentQuestions = (item \ "components").as[JsObject]
       val question = (componentQuestions \ id).as[JsObject]
       val componentType = (question \ "componentType").as[String]
@@ -29,7 +29,7 @@ class OutcomeProcessorImpl(components: Seq[UiComponent], libraries : Seq[Library
 
           answer.map {
             a =>
-              val componentLibraries : Seq[Library] = component.libraries.map( id => libraries.find(l => l.id.matches(id) )).flatten
+              val componentLibraries: Seq[Library] = component.libraries.map(id => libraries.find(l => l.id.matches(id))).flatten
               val generator = new OutcomeGenerator(component.componentType, component.server.definition, componentLibraries)
 
               (id, generator.createOutcome(question, a, settings, targetOutcome))
@@ -43,15 +43,20 @@ class OutcomeProcessorImpl(components: Seq[UiComponent], libraries : Seq[Library
 
     val componentQuestions = (item \ "components").as[JsObject]
 
-    val responsesWithoutTarget: Seq[(String, JsValue)] = componentQuestions.keys.toSeq.filter(getTargetIdFor(_).isEmpty).map(createOutcomeForComponent(_, JsObject(Seq.empty)))
-    val responsesWithTarget: Seq[(String, JsValue)] = componentQuestions.keys.toSeq.filter(!getTargetIdFor(_).isEmpty).map {
-      id=>
+    val componentsGroupedByTargetExist = componentQuestions.keys.toSeq.groupBy {
+      g =>
+        Map(true -> "withoutTarget", false -> "withTarget").get(getTargetIdFor(g).isEmpty).getOrElse("")
+    }
+
+    val outcomesWithoutTarget: Seq[(String, JsValue)] = componentsGroupedByTargetExist.get("withoutTarget").getOrElse(Seq()).map(createOutcomeForComponent(_, JsObject(Seq.empty)))
+    val outcomesWithTarget: Seq[(String, JsValue)] = componentsGroupedByTargetExist.get("withTarget").getOrElse(Seq()).map {
+      id =>
         val targetId = getTargetIdFor(id).get
-        val existingOutcome = responsesWithoutTarget.find(_._1 == targetId).map(_._2).getOrElse(JsObject(Seq.empty))
+        val existingOutcome = outcomesWithoutTarget.find(_._1 == targetId).map(_._2).getOrElse(JsObject(Seq.empty))
         createOutcomeForComponent(id, existingOutcome)
     }
 
-    JsObject(responsesWithoutTarget ++ responsesWithTarget)
+    JsObject(outcomesWithoutTarget ++ outcomesWithTarget)
   }
 
   private def getAnswer(answers: JsValue, id: String): Option[JsValue] = for {
