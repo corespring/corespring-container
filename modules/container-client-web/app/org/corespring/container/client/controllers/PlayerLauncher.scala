abf35a901bb944565535e75dd10c489a86c19bf1
edeustace <ed.eustace@gmail.com>
2014-01-08 13:43:56 +0000

    Added 'playerPage' optional query for player.js to allow a different player to be used

--- modules/container-client-web/app/org/corespring/container/client/controllers/PlayerLauncher.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/PlayerLauncher.scala
@@ -10,20 +10,49 @@ import play.api.mvc.{AnyContent, Request, Action, Controller}
 
 trait PlayerLauncher extends Controller {
 
+  //TODO: This is lifted from corespring-api -> move to a library
+  object BaseUrl {
+    def apply(r: Request[AnyContent]): String = {
+
+      /**
+       * Note: You can't check a request to see if its http or not in Play
+       * But even if you could you may be sitting behind a reverse proxy.
+       * @see: https://groups.google.com/forum/?fromgroups=#!searchin/play-framework/https$20request/play-framework/11zbMtNI3A8/o4318Z-Ir6UJ
+       * but the tip was to check for the header below
+       */
+      val protocol = r.headers.get("x-forwarded-proto") match {
+        case Some("https") => "https"
+        case _ => "http"
+      }
+
+      protocol + "://" + r.host
+    }
+  }
+
+  import org.corespring.container.client.controllers.hooks.routes.PlayerHooks
+  import org.corespring.container.client.controllers.routes.Assets
+
   val SecureMode = "corespring.player.secure"
 
   def isSecure(r:Request[AnyContent]) : Boolean
 
-  //TODO: Move to container-client-web or its own module - as we'll be reusing this
+  /**
+   * query: playerPage the player page to load (default: index.html), for a simple player you can pass in container-player.html
+   */
   def playerJs = Action{ request =>
 
-    val rootUrl = play.api.Play.current.configuration.getString("APP_ROOT_URL").getOrElse("http://localhost:9000")
+    val playerPage = request.getQueryString("playerPage").getOrElse("index.html")
+
+    val rootUrl = play.api.Play.current.configuration.getString("APP_ROOT_URL").getOrElse( BaseUrl(request) )
+
+    val itemUrl = s"${PlayerHooks.createSessionForItem(":id").url}?file=$playerPage"
+    val sessionUrl = s"${Assets.session(":id", playerPage)}"
 
     val defaultOptions =
       s"""
         |exports.corespringUrl = "$rootUrl";
-        |exports.itemPath = "${org.corespring.container.client.controllers.hooks.routes.PlayerHooks.createSessionForItem(":id").url}";
-        |exports.sessionPath = "${org.corespring.container.client.controllers.routes.Assets.session(":id", "index.html")}";
+        |exports.itemPath = "$itemUrl";
+        |exports.sessionPath = "$sessionUrl";
         |exports.mode = "gather";
       """.stripMargin
 
@@ -40,7 +69,6 @@ trait PlayerLauncher extends Controller {
       Play.resource(p).map {
         r =>
           val name = new File(r.getFile).basename.getName.replace(".js", "")
-          val file = Play.getFile(r.getPath)
           val contents = scala.io.Source.fromInputStream(r.getContent().asInstanceOf[InputStream]).getLines.mkString("\n")
           (name, contents)
       }.getOrElse((p, ""))
