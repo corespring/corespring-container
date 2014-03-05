8204e742379735851ce622aff1c04640b8eb5359
edeustace <ed.eustace@gmail.com>
2014-03-05 12:16:03 +0000

    some tidy up

--- modules/container-client/src/main/resources/container-client/js/editor/config.js
+++ modules/container-client/src/main/resources/container-client/js/editor/config.js
@@ -1,32 +1,3 @@
 (function(){
-
-  var config = [
-/*
-  '$routeProvider', 
-  '$locationProvider',
-  function($routeProvider, $locationProvider){
-    $locationProvider.html5Mode(false);
-    $routeProvider.
-      when('/profile', {
-        //templateUrl: 'profile.html',
-        controller: 'Profile'
-      }).
-      when('/designer', {
-        //templateUrl: 'designer.html',
-        controller: 'Designer'
-      }).
-      when('/supporting-materials', {
-        //templateUrl: 'supporting-materials.html',
-        controller: 'SupportingMaterials'
-      }).
-      otherwise({
-        redirectTo: '/profile'
-      });
-
-  }
- */
-];
-
-corespring.module("corespring-editor-config", config);
-
+corespring.module("corespring-editor-config", []);
 })();
