fc5d48b84b85ac08051b376d11b420e76b8b3ddf
edeustace <ed.eustace@gmail.com>
2014-03-25 11:00:42 +0000

    polish preview show/hide

--- modules/container-client/src/main/resources/container-client/js/editor/controllers/root.js
+++ modules/container-client/src/main/resources/container-client/js/editor/controllers/root.js
@@ -1,32 +1,47 @@
-var controller = function($scope, $log, $location, DataQueryService, ItemService, NavModelService) {
+var controller = function($scope, $log, $location, $timeout, DataQueryService, ItemService, NavModelService) {
+
+  $scope.nav = NavModelService;
 
   var previewable = [
     '/design',
     '/item-profile'
   ];
 
-  var search = $location.search();
-  $scope.showLeftNav = search.leftnav === true || search.leftnav === 'true';
-  $scope.showPreview = search.preview === true || search.preview === 'true';
+  function showPreview() {
+    var search = $location.search();
+    return search.preview === true || search.preview === 'true';
+  }
+
+  function showLeftNav() {
+    var search = $location.search();
+    return search.leftnav === true || search.leftnav === 'true';
+  }
+
+  $timeout(function() {
+    var search = $location.search();
+    $scope.showLeftNav = showLeftNav();
+    $scope.showPreview = showPreview();
+  });
 
   $scope.toggleLeftNav = function(updateLocation) {
-    //updateLocation = updateLocation || true;
-    $scope.showLeftNav = !$scope.showLeftNav;
-    $location.search('leftnav', $scope.showLeftNav);
+    var show = showLeftNav();
+    $location.search('leftnav', !show);
+    $scope.showLeftNav = !show;
   };
 
   $scope.togglePreview = function() {
-    $scope.showPreview = !$scope.showPreview;
-    $location.search('preview', $scope.showPreview);
+    if (_.contains(previewable, $location.path())) {
+      var show = showPreview();
+      $location.search('preview', !show);
+      $scope.showPreview = !show;
+    }
   };
-
-  $scope.nav = NavModelService;
 
   $scope.$on('$locationChangeSuccess', function() {
     NavModelService.chooseNavEntry($location.path());
 
     if (_.contains(previewable, $location.path())) {
-      $scope.showPreview = true;
+      $scope.showPreview = showPreview();
     } else {
       $scope.showPreview = false;
     }
@@ -75,6 +90,7 @@ angular.module('corespring-editor.controllers')
   .controller('Root', ['$scope',
     '$log',
     '$location',
+    '$timeout',
     'DataQueryService',
     'ItemService',
     'NavModelService',
