c413ce500f7f20ec4ca2fc3e1ede119be692a815
benburton <benjamin.joseph.burton@gmail.com>
2014-04-09 15:34:25 -0400

    Remove unused data

--- modules/container-client/src/main/resources/container-client/js/editor/services/nav-model-service.js
+++ modules/container-client/src/main/resources/container-client/js/editor/services/nav-model-service.js
@@ -1,35 +1,12 @@
 (function(){
   function NavModelService ($location, $rootScope) {
 
-    this.navEntries = [
-      {
-        title: "Design",
-        path: "design"
-      },
-      {
-        title: "View Player",
-        path: "view-player"
-      },
-      {
-        title: "Item Profile",
-        path: "item-profile"
-      },
-      {
-        title: "Supporting Materials",
-        path: "supporting-materials"
-      },
-      {
-        title: "Overview",
-        path: "overview"
-      }
-    ];
-
-
     // TODO Remove when this https://github.com/angular-ui/ui-router/issues/202 is resolved.
     (function() {
       $rootScope.$on('$stateChangeStart', function() { this.locationSearch = $location.search(); });
       $rootScope.$on('$stateChangeSuccess', function() { $location.search(this.locationSearch); });
     })();
+
   }
 
   angular.module('corespring-editor.services')
