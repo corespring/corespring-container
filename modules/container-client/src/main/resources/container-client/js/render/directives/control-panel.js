69440c6d3ba5e7d601636d8dea59b8adf59ce528
benburton <benjamin.joseph.burton@gmail.com>
2014-05-08 11:12:36 -0400

    Switch modes to toggle buttons, add score

--- modules/container-client/src/main/resources/container-client/js/render/directives/control-panel.js
+++ modules/container-client/src/main/resources/container-client/js/render/directives/control-panel.js
@@ -19,24 +19,39 @@
   angular.module('corespring-player.directives').directive('playerControlPanel', [
     function() {
       var link = function($scope, $element) {
+        var $configLink = $element.find('.action.config');
+
         console.log("player control panel");
+
         $scope.showSettings = false;
+        $scope.mode = 'gather';
 
         $scope.reset = function() {
           $scope.$broadcast('resetPreview');
         };
 
-        $element.find('.action.config').popover({
+        $scope.$watch('mode', function(mode) {
+          if (mode === 'feedback') {
+            $configLink.popover('enable');
+          } else {
+            $configLink.popover('disable');
+          }
+        });
+
+        $element.on('click', '.dismiss-popover', function() {
+          $('.action.config').popover('hide');
+        });
+
+        $configLink.popover({
           html: true,
           placement: 'bottom',
           content: [
             '<ul class="settings">',
                radioOrCheckbox("highlightUserResponse", "Highlight user outcome"),
                radioOrCheckbox("highlightCorrectResponse", "Highlight correct outcome"),
-               radioOrCheckbox("showFeedback", "Show feedback"),
                radioOrCheckbox("allowEmptyResponses", "Allow empty responses"),
             '</ul>',
-            '<a class="btn btn-success btn-small btn-sm" onClick="$(\'.action.config\').popover(\'hide\');">Done</a>'
+            '<a class="btn btn-success btn-small btn-sm dismiss-popover">Done</a>'
           ].join('\n')
         });
 
@@ -47,14 +62,16 @@
         link: link,
         template: [
           '<div class="control-panel">',
-          '  <label>Mode:</label>',
-          '  <select>',
-          '    <option>Feedback</option>',
-          '  </select>',
-          '  <label>Score:</label>',
-          '  <span>{{score.summary.percentage}}%</span>',
+          '  <div class="btn-group">',
+          '    <label class="btn btn-success" ng-model="mode" btn-radio="\'gather\'">Gather</label>',
+          '    <label class="btn btn-success" ng-model="mode" btn-radio="\'feedback\'">Feedback</label>',
+          '  </div>',
+          '  <div class="score" ng-show="score">',
+          '    <label>Score:</label>',
+          '    <span>{{score.summary.percentage}}%</span>',
+          '  </div>',
           '  <ul class="actions">',
-          '    <li class="action config"><a title="Settings" /></li>',
+          '    <li class="action config"><a ng-class="{disabled: mode == \'gather\'}" title="Settings"><img src="../../images/settings-icon.png"/></a></li>',
           '    <li class="action reset"><a title="Reset" ng-click="reset()" /></li>',
           '  </ul>',
           '</div>'
