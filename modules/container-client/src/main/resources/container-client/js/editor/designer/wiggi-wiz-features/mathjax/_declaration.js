7b458185bdfdae1b0d78b9d472979a114f042c0d
benburton <benjamin.joseph.burton@gmail.com>
2014-05-20 14:12:39 -0400

    Keep your vars to yourself.

--- modules/container-client/src/main/resources/container-client/js/editor/designer/wiggi-wiz-features/mathjax/_declaration.js
+++ modules/container-client/src/main/resources/container-client/js/editor/designer/wiggi-wiz-features/mathjax/_declaration.js
@@ -1,6 +1,5 @@
-var module;
 try {
-  module = angular.module('corespring.wiggi-wiz-features');
+  angular.module('corespring.wiggi-wiz-features');
 } catch (e) {
   angular.module('corespring.wiggi-wiz-features', []);
 }
