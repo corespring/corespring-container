56ab4f95491f87a77a0ecbbeb408b159f70a1723
rbokel <rbokel.github@qlod.org>
2014-05-28 18:39:54 +0200

    add

--- modules/container-client/src/main/resources/container-client/js/common/directives/summary-feedback.js
+++ modules/container-client/src/main/resources/container-client/js/common/directives/summary-feedback.js
@@ -1,22 +1,45 @@
 angular.module('corespring-common.directives').directive('summaryFeedback', ['$log',
   function($log) {
     return {
+      scope : {
+        data: "=",
+        item: "=",
+        session: "=",
+        template: "@"
+      },
       restrict: 'E',
       replace: true,
       template: [
-        '<div class="summary-feedback" ng-show="data.item.summaryFeedback && session.isComplete">',
+        '<div class="summary-feedback" ng-show="summaryFeedback && isComplete">',
         '  <a class="show-feedback-button" ng-click="toggleSummaryFeedbackOpen()">',
         '    <i class="fa fa-{{isSummaryFeedbackOpen ? \'minus\' : \'plus\'}}-square-o"></i>',
         '    <span>Click for more feedback</span>',
         '  </a>',
         '  <div class="feedback-text" ng-show="isSummaryFeedbackOpen">',
-        '    {{data.item.summaryFeedback}}',
+        '    {{summaryFeedback}}',
         '  </div>',
         '</div>'
       ].join(""),
       link: function($scope, iElm, iAttrs, controller) {
 
-        var log = $log.debug.bind($log, '[summaryFeedback directive] - ');
+        var log = $log.debug.bind($log, '[summaryFeedback directive from ' + $scope.template + '] - ');
+
+        log("setup");
+
+        $scope.$watch("item.summaryFeedback", function(value){
+          log("watch item.summaryFeedback", value);
+          $scope.summaryFeedback = value;
+        });
+
+        $scope.$watch("data.item.summaryFeedback", function(value){
+          log("watch data.item.summaryFeedback", value);
+          $scope.summaryFeedback = value;
+        });
+
+        $scope.$watch("session.isComplete", function(value){
+          log("watch session.isComplete", value);
+          $scope.isComplete = value;
+        });
 
         $scope.isSummaryFeedbackOpen = false;
 
