9edb50d66bcaaaea6e312d63d6bf2002230a49b3
Attila Miklosi <attila.miklosi@gmail.com>
2014-06-23 18:50:41 +0200

    added test for zero weight score

--- modules/component-model/src/test/scala/org/corespring/container/components/outcome/DefaultScoreProcessorTest.scala
+++ modules/component-model/src/test/scala/org/corespring/container/components/outcome/DefaultScoreProcessorTest.scala
@@ -69,6 +69,31 @@ class DefaultScoreProcessorTest extends Specification {
       (item, responses) must GenerateOutcome(expected)
     }
 
+    "zero weight is not counted in score" in {
+
+      val item = """{
+             "components": {
+               "1" : {"weight":1},
+               "2" : {"weight":0}
+             }
+           }"""
+      val responses = """{
+             "1" : {"score":0.0},
+             "2" : {"score":1.0}
+            }"""
+      val expected = """
+         {
+           "summary" : { "maxPoints" : 1, "points" : 0.0, "percentage" : 0.0 },
+           "components" : {
+             "1" : { "weight" : 1, "score" : 0.0, "weightedScore" : 0.0},
+             "2" : { "weight" : 0, "score" : 1.0, "weightedScore" : 0.0}
+           }
+         }
+                     """
+
+      (item, responses) must GenerateOutcome(expected)
+    }
+
     "calculate the proper sum" in {
       DefaultScoreProcessor.getSumOfWeightedScores(
         Json.obj(
@@ -92,7 +117,7 @@ class DefaultScoreProcessorTest extends Specification {
       JsonCompare.caseInsensitiveSubTree(Json.stringify(outcome), expectedOutcome) match {
         case Right(_) => true
         case Left(diffs) => {
-          println(diffs.mkString("\n"))
+          println(diffs.mkString("KAKAMATYI\n"))
           false
         }
       }
