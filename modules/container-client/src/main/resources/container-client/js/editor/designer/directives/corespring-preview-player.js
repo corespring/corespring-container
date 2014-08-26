27c4d4fb03003d970eb0302cbac90c9f1c152bbe
Attila Miklosi <attila.miklosi@gmail.com>
2014-08-26 15:56:05 +0200

    fix invalid scope apply

--- modules/container-client/src/main/resources/container-client/js/editor/designer/directives/corespring-preview-player.js
+++ modules/container-client/src/main/resources/container-client/js/editor/designer/directives/corespring-preview-player.js
@@ -28,7 +28,10 @@ angular.module('corespring-editor.directives').directive('corespringPreviewPlaye
         if (getComponentById(id).hasClass('component-container')) {
           getComponentById(id).addClass('selected');
           $scope.selectedComponentId = id;
-          $scope.$apply();
+          var phase = $scope.root && $scope.$root.$$phase;
+          if (phase && phase !== '$apply' && phase !== '$digest') {
+            $scope.$apply();
+          }
 
           if ($('.component-container.selected').size() > 0) {
             var target = $('.component-container.selected')[0];
