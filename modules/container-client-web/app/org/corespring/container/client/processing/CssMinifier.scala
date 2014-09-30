5d7071094ba8ea567bb605e3542d007e860abfe3
Ed Eustace <ed.eustace@gmail.com>
2014-09-30 13:06:25 +0100

    Close StringReader + StringWriter

--- modules/container-client-web/app/org/corespring/container/client/processing/CssMinifier.scala
+++ modules/container-client-web/app/org/corespring/container/client/processing/CssMinifier.scala
@@ -1,15 +1,21 @@
 package org.corespring.container.client.processing
 
-import java.io.{ StringReader, StringWriter }
+import java.io.{StringReader, StringWriter}
 
 trait CssMinifier {
 
-  def minifyCss(contents: String): Either[String, String] = try {
-    val compressor = new com.yahoo.platform.yui.compressor.CssCompressor(new StringReader(contents))
+  def minifyCss(contents: String): Either[String, String] = {
+    val reader = new StringReader(contents)
     val writer = new StringWriter()
-    compressor.compress(writer, 0)
-    Right(writer.toString)
-  } catch {
-    case e: Throwable => Left(e.getMessage)
+    try {
+      val compressor = new com.yahoo.platform.yui.compressor.CssCompressor(reader)
+      reader.close()
+      compressor.compress(writer, 0)
+      val out = writer.toString
+      writer.close()
+      Right(out)
+    } catch {
+      case e: Throwable => Left(e.getMessage)
+    }
   }
 }
