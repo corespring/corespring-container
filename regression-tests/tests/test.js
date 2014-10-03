7a94a7a5a732842c45e266b8925c715cd6184b22
rbokel <rbokel.github@qlod.org>
2014-10-03 17:18:25 +0200

    fix regression test

--- regression-tests/tests/test.js
+++ regression-tests/tests/test.js
@@ -1,12 +1,13 @@
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 
 describe('corespring-container', function() {
 
   it('does have link to create an item', function(done) {
     browser
-      .url(regressionTestRunnerGlobals.getUrl("/"))
+      .url(regressionTestRunnerGlobals.getUrl(""))
       .isVisible('a[href="/create-item"]', function(err, result) {
-        expect(err).to.be.null;
+        assert.isUndefined(err, "Unexpected error " + JSON.stringify(err) + " at url <" + regressionTestRunnerGlobals.getUrl("") + ">");
         expect(result).not.to.be.null;
       })
       .call(done);
