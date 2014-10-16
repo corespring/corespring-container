30f11e09159f4b00b740812ff266dde163fe12fe
Attila Miklosi <attila.miklosi@gmail.com>
2014-10-16 18:07:21 +0200

    CA-2258: multiple-choice server side js - correctResponses is undefined in preprocessItem

--- modules/container-client-web/app/org/corespring/container/client/controllers/resources/Session.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/resources/Session.scala
@@ -91,9 +91,13 @@ trait Session extends Controller with ItemPruner with HasContext {
       val json = fs.everything
 
       val itemJson = (json \ "item").as[JsObject]
-      val prunedItem = pruneItem(itemJson)
+
+      //Note: We must run the itemProcessor with the complete data model as it depends on it.
+      val processedItem = itemPreProcessor.preProcessItemForPlayer(itemJson)
+
+      //Note: we can now run the regular prune ...
+      val prunedItem = pruneItem(processedItem)
       val sessionJson = (json \ "session").as[JsObject]
-      val processedItem = itemPreProcessor.preProcessItemForPlayer(prunedItem)
 
       val base = Json.obj(
         "item" -> processedItem,
