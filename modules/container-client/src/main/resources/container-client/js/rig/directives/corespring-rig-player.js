221e1cbb9bb0650c33d58b7ce783676eb551732f
dimabur <dimabur@gmail.com>
2014-10-28 19:40:04 +0200

    Fixed Rig update

---
+++ modules/container-client/src/main/resources/container-client/js/rig/directives/corespring-rig-player.js
@@ -0,0 +1,8 @@
+angular.module('corespring-rig.directives').directive('corespringRigPlayer', [
+  '$log',
+  'CorespringPlayerDefinition',
+  function($log, CorespringPlayerDefinition) {
+    return new CorespringPlayerDefinition({
+      mode: 'rig'
+    });
+  }]);
