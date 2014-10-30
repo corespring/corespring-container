4780772b5316bd84ef15432262ac03eeeffa1256
rbokel <rbokel.github@qlod.org>
2014-10-30 17:41:31 +0100

    CA-2090 use for-comprehension for getting the new relic config

--- modules/container-client-web/app/org/corespring/container/client/V2PlayerConfig.scala
+++ modules/container-client-web/app/org/corespring/container/client/V2PlayerConfig.scala
@@ -17,14 +17,16 @@ class V2PlayerConfig(val underlying: Option[Configuration]) {
 
   lazy val newRelicRumConfig: Option[JsValue] = {
     import NewRelicRumConfig.writes
-    underlying.map { c =>
-      c.getBoolean("newrelic.enabled") match {
-        case Some(true) => Some(Json.toJson(NewRelicRumConfig(
-          licenseKey = c.getString("newrelic.license-key").getOrElse(""),
-          applicationID = c.getString("newrelic.application-id ").getOrElse(""))))
-        case _ => None
-      }
-    }.getOrElse(None)
+
+    for {
+      map <- underlying
+      enabled <- map.getBoolean("newrelic.enabled")
+      if enabled
+      licenseKey <- map.getString("newrelic.license-key")
+      applicationID <- map.getString("newrelic.application-id ")
+    } yield {
+      Json.toJson(NewRelicRumConfig(licenseKey, applicationID))
+    }
   }
 }
 
