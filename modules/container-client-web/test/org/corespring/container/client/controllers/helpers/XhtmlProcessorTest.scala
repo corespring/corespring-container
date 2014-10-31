8daa9903308a8b5f98244e850c3db3cbb70b8bfc
dimabur <dimabur@gmail.com>
2014-10-31 17:24:54 +0200

    CA-2297 interactions not showing in - line CA-2298 tabs not rendering

--- modules/container-client-web/test/org/corespring/container/client/controllers/helpers/XhtmlProcessorTest.scala
+++ modules/container-client-web/test/org/corespring/container/client/controllers/helpers/XhtmlProcessorTest.scala
@@ -15,9 +15,14 @@ class XhtmlProcessorTest extends Specification with XhtmlProcessor {
     "process" in {
 
       val xhtml = "<div><corespring-apple id='1'></corespring-apple><corespring-banana id='2'></corespring-banana></div>"
-
       tagNamesToAttributes(xhtml).get.trim.toUnix ===
-        """<div><div corespring-apple="corespring-apple" id="1"></div><div corespring-banana="corespring-banana" id="2"></div></div>""".toUnix
+        """<div><div corespring-apple="corespring-apple" id='1'></div><div corespring-banana="corespring-banana" id='2'></div></div>""".toUnix
+    }
+
+    "keep nested tag structure intact" in {
+      val xhtml = "<corespring-one><corespring-two>Content</corespring-two></corespring-one>"
+      tagNamesToAttributes(xhtml).get.trim.toUnix ===
+        """<div corespring-one="corespring-one"><div corespring-two="corespring-two">Content</div></div>""".toUnix
     }
   }
 
