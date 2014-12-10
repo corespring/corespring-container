745d7749f1dcafe75c94bd6c035d438103a2399d
benburton <benjamin.joseph.burton@gmail.com>
2014-12-10 17:14:17 -0500

    Get dev editor to persist changes

--- modules/container-client/src/js/dev-editor/controllers/dev-editor-root.js
+++ modules/container-client/src/js/dev-editor/controllers/dev-editor-root.js
@@ -1,8 +1,32 @@
 angular.module('corespring-dev-editor.controllers')
   .controller('DevEditorRoot', [
     '$scope',
-    function($scope) {
-      $scope.title = "Hello, world.";
+    'ItemIdService',
+    'ItemService',
+    function($scope, ItemIdService, ItemService) {
+      $scope.itemId = ItemIdService.itemId();
+
+      $scope.onItemLoaded = function(item) {
+        $scope.item = item;
+        $scope.xhtml = item.xhtml;
+        $scope.json = JSON.stringify(item.components, undefined, 2);
+      };
+
+      $scope.save = function() {
+        $scope.item.xhtml = $scope.xhtml;
+        $scope.item.components = JSON.parse($scope.json);
+        ItemService.save($scope.item, function() {
+          window.alert('success!');
+        }, function() {
+          window.alert('failure!');
+        }, $scope.itemId);
+      };
+
+      $scope.onItemLoadError = function(err) {
+        window.alert("There was an error. Please try later. Thanks!");
+      };
+
+      ItemService.load($scope.onItemLoaded, $scope.onItemLoadError, $scope.itemId);
     }
   ]
 );
