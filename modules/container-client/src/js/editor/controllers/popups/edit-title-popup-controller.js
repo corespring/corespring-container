458ec8b7051e97840e49dbd01740a4bde5d8ab75
edeustace <ed.eustace@gmail.com>
2014-12-16 13:49:33 +0000

    add ellipsis for title

--- modules/container-client/src/js/editor/controllers/popups/edit-title-popup-controller.js
+++ modules/container-client/src/js/editor/controllers/popups/edit-title-popup-controller.js
@@ -1,14 +1,18 @@
 angular.module('corespring-editor.controllers').controller('EditTitlePopupController', [
   '$scope',
-  'LogFactory',
   '$modalInstance',
   'title',
-  function($scope, LogFactory, $modalInstance, title){
+  function($scope, $modalInstance, title){
 
     $scope.title = title;
 
     $scope.ok = function(){
-      $modalInstance.close($scope.title);
+      /**
+      Note: for some reason the $scope provided has a child scope that is bound to the view
+      We are getting the title from the child until we figure out why this is happening.
+      */
+      var t = $scope.$$childTail.title;
+      $modalInstance.close(t);
     };
 
     $scope.cancel = function(){
