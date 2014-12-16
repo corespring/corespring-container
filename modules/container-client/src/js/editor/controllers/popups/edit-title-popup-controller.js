ef1a6cd48eb58ac02900f14c8d1482ca176186de
edeustace <ed.eustace@gmail.com>
2014-12-16 14:53:05 +0000

    use .title

--- modules/container-client/src/js/editor/controllers/popups/edit-title-popup-controller.js
+++ modules/container-client/src/js/editor/controllers/popups/edit-title-popup-controller.js
@@ -7,12 +7,7 @@ angular.module('corespring-editor.controllers').controller('EditTitlePopupContro
     $scope.title = title;
 
     $scope.ok = function(){
-      /**
-      Note: for some reason the $scope provided has a child scope that is bound to the view
-      We are getting the title from the child until we figure out why this is happening.
-      */
-      var t = $scope.$$childTail.title;
-      $modalInstance.close(t);
+      $modalInstance.close($scope.title);
     };
 
     $scope.cancel = function(){
