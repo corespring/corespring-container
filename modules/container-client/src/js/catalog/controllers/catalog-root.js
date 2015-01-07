10c572ac8900e79b8b9018bdda1e1875922668ae
Ed Eustace <ed.eustace@gmail.com>
2015-01-07 12:58:44 +0000

    prep server for @mikl

--- modules/container-client/src/js/catalog/controllers/catalog-root.js
+++ modules/container-client/src/js/catalog/controllers/catalog-root.js
@@ -1,13 +1,22 @@
 angular.module('corespring-catalog.controllers')
   .controller('CatalogRoot', [
-    '$scope', '$log', 'SupportingMaterialsService',
-    function($scope, $log, SupportingMaterialsService) {
+    '$scope', 'LogFactory', 'SupportingMaterialsService', 'ItemService',
+    function($scope, LogFactory, SupportingMaterialsService, ItemService) {
 
-      var log = $log.debug.bind($log, '[catalog root] -');
+      var log = LogFactory.getLogger('CatalogRoot');
 
       $scope.$on('itemLoaded', function(ev, item) {
         $scope.supportingMaterials = SupportingMaterialsService.getSupportingMaterialsByGroups(item.supportingMaterials);
       });
+
+      $scope.onLoaded = function(){
+        log.debug('loaded', arguments);
+      };
+      $scope.onLoadFailed = function(){
+        log.debug('load failed', arguments);
+      };
+
+      ItemService.load($scope.onLoaded, $scope.onUploadFailed);
     }
 
   ]);
