95d1b835b3a6016a4181f24b6a57f266a9737921
Attila Miklosi <attila.miklosi@gmail.com>
2015-01-16 14:03:26 +0100

    disabling vertical scrollbar for the player to avoid infinite resizing loop due to rewrapping of item text changed dimension watcher to be more frequent after item rendering then less frequent once things stabilised

--- modules/container-client/src/js/render/directives/dimension-propagator.js
+++ modules/container-client/src/js/render/directives/dimension-propagator.js
@@ -1,45 +1,56 @@
 angular.module('corespring-player.directives').directive('dimensionPropagator',
   ['$log', 'Msgr', function($log, Msgr) {
 
-  function calculateContentHeight(){
-    var visible = $( "body :visible" );
-    var max = 0;
-    visible.each(function(){
-      var rect = this.getBoundingClientRect();
-      max = Math.max(rect.bottom,max);
-    });
-    return max;
-  }
-
-  return {
-
-    link: function($scope, $element){
-
-      var $body = $element.parents('body');
-
-      var lastW;
-      var lastH;
-
-      function different(w,h){
-        return lastW !== w || lastH !== h;
-      }
-
-      function dispatchDimensions(){
-        var b = $body[0];
-        if (!b) {return;}
-
-        var w = b.clientWidth;
-        var h = calculateContentHeight();
-
-        if(different(w,h)){
-          lastW = w;
-          lastH = h;
-          Msgr.send('dimensionsUpdate',{w:w, h:h});
-        }
-      }
-
-      setInterval(dispatchDimensions, 400);
-      dispatchDimensions();
+    function calculateContentHeight() {
+      var visible = $("body :visible");
+      var max = 0;
+      visible.each(function() {
+        var rect = this.getBoundingClientRect();
+        max = Math.max(rect.bottom, max);
+      });
+      return max;
     }
-  };
-}]);
+
+    return {
+
+      link: function($scope, $element) {
+
+        var $body = $element.parents('body');
+
+        var lastW, lastH;
+        var counter = 0;
+        var highFrequencyDelay = 10;
+        var lowFrequencyDelay = 100;
+        var frequencyChangesAt = 2000;
+
+        function different(w, h) {
+          return lastH !== h || lastW !== w;
+        }
+
+        function dispatchDimensions() {
+          var b = $body[0];
+          if (!b) {
+            return;
+          }
+
+          var w = b.clientWidth;
+          var h = calculateContentHeight();
+
+          if (different(w, h)) {
+            lastW = w;
+            lastH = h;
+            Msgr.send('dimensionsUpdate', {w: w, h: h});
+
+            counter = 0;
+          }
+          var delay = (counter * highFrequencyDelay < frequencyChangesAt) ? highFrequencyDelay : lowFrequencyDelay;
+          counter = counter + 1;
+          clearTimeout(timeout);
+          timeout = setTimeout(dispatchDimensions, delay);
+        }
+
+        var timeout = setTimeout(dispatchDimensions, 1);
+        dispatchDimensions();
+      }
+    };
+  }]);
