bc9fa36c29ef653b60e531e06da0f65c98d97f4e
Ed Eustace <ed.eustace@gmail.com>
2015-01-22 18:34:26 +0000

    try to read in the version info for the components

--- modules/container-client-web/app/org/corespring/container/client/VersionInfo.scala
+++ modules/container-client-web/app/org/corespring/container/client/VersionInfo.scala
@@ -1,9 +1,11 @@
 package org.corespring.container.client
 
+import java.io.File
 import java.util.Properties
+import org.apache.commons.io.FileUtils
 import play.api.Play
 import play.api.Play.current
-import play.api.libs.json.Json
+import play.api.libs.json.{ JsNull, JsValue, JsObject, Json }
 
 case class VersionInfo(version: String, branch: String, commitHash: String, date: String)
 
@@ -12,11 +14,15 @@ object VersionInfo {
     new VersionInfo(version, branch, commitHashShort, pushDate)
   }
 
-  def json = Json.obj(
-    "version" -> version,
-    "commitHash" -> commitHashShort,
-    "branch" -> branch,
-    "date" -> pushDate)
+  def json = {
+    val compInfo: JsValue = components.getOrElse(JsNull)
+    Json.obj(
+      "version" -> version,
+      "commitHash" -> commitHashShort,
+      "branch" -> branch,
+      "date" -> pushDate,
+      "corespring-components" -> compInfo)
+  }
 
   val propsFile = "/container-client-web-buildInfo.properties"
 
@@ -30,6 +36,19 @@ object VersionInfo {
         input.close()
         props
     }.getOrElse(new Properties())
+  }
+
+  private lazy val components: Option[JsValue] = {
+    for {
+      path <- Play.current.configuration.getString("components.path")
+      comps <- {
+        val f = new File(path)
+        if (f.exists) Some(f) else None
+      }
+      versionFile <- comps.getParentFile.listFiles().find(_.getName == "version-info.json")
+      jsonString <- Some(FileUtils.readFileToString(versionFile))
+      json <- Some(Json.parse(jsonString))
+    } yield json
   }
 
   private lazy val commitHashShort: String = properties.getProperty("commit.hash", "?")
