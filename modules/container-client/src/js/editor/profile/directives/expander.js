4465182b4152f36634182a5093d33e6737794dd5
benburton <benjamin.joseph.burton@gmail.com>
2015-01-28 16:08:40 -0500

    Add test for expander

--- modules/container-client/src/js/editor/profile/directives/expander.js
+++ modules/container-client/src/js/editor/profile/directives/expander.js
@@ -5,13 +5,8 @@ angular.module('corespring-editor.directives').directive('expander', [
   'LogFactory',
   function(LogFactory){
     var logger = LogFactory.getLogger('expander');
-    function link($scope, $element, $attrs){
-      logger.debug('link!');
-    }
-
     return {
       restrict: 'E',
-      link: link,
       scope: {
         title: '@'
       },
