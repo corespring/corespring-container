09655801d6f9dfce004592bfcf7f273bb841a4c0
dimabur <dimabur@gmail.com>
2015-01-29 19:57:43 +0200

    Added jshint ignore to the line with IE8 compatibilty issue

--- modules/container-client/src/js/common/services/iframe.js
+++ modules/container-client/src/js/common/services/iframe.js
@@ -7,7 +7,7 @@ angular.module('corespring-common.services').service('iFrameService', [
         _top = _top ? _top : top;
         _window = _window ? _window : window;
         /** note use != to support ie8 instead of !== */
-        return _top !== _window;
+        return _top != _window; // jshint ignore:line
       };
 
     }
