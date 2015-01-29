ac49a4622d2943ea91e9e1bf472ea5a4a5b0379a
edeustace <ed.eustace@gmail.com>
2015-01-29 15:20:48 +0000

    fix formatting

--- modules/container-client/src/js/editor/controllers/question.js
+++ modules/container-client/src/js/editor/controllers/question.js
@@ -1,170 +1,170 @@
 angular.module('corespring-editor.controllers')
-	.controller('QuestionController', [
-		'$scope',
-		'$element',
-		'$timeout',
-		'ItemService',
-		'EditorConfig',
-		'LogFactory',
-		'ComponentImageService',
-		'ComponentData',
-		'ComponentPopups',
-		'AppState',
-		'ScoringHandler',
-		'MathJaxService',
-		function($scope,
-			$element,
-			$timeout,
-			ItemService,
-			EditorConfig,
-			LogFactory,
-			ComponentImageService,
-			ComponentData,
-			ComponentPopups,
-			AppState,
-			ScoringHandler,
-			MathJaxService) {
+  .controller('QuestionController', [
+    '$scope',
+    '$element',
+    '$timeout',
+    'ItemService',
+    'EditorConfig',
+    'LogFactory',
+    'ComponentImageService',
+    'ComponentData',
+    'ComponentPopups',
+    'AppState',
+    'ScoringHandler',
+    'MathJaxService',
+    function($scope,
+      $element,
+      $timeout,
+      ItemService,
+      EditorConfig,
+      LogFactory,
+      ComponentImageService,
+      ComponentData,
+      ComponentPopups,
+      AppState,
+      ScoringHandler,
+      MathJaxService) {
 
-			var configPanels = {};
+      var configPanels = {};
 
-			var logger = LogFactory.getLogger('question-controller');
+      var logger = LogFactory.getLogger('question-controller');
 
-			$scope.previewOn = AppState.question.preview || false;
+      $scope.previewOn = AppState.question.preview || false;
 
-			$scope.$watch('previewOn', function() {
-				AppState.question.preview = $scope.previewOn;
-			});
+      $scope.$watch('previewOn', function() {
+        AppState.question.preview = $scope.previewOn;
+      });
 
-			$scope.showSummaryFeedback = false;
+      $scope.showSummaryFeedback = false;
 
-			$scope.togglePreview = function() {
-				$scope.previewOn = !$scope.previewOn;
-			};
+      $scope.togglePreview = function() {
+        $scope.previewOn = !$scope.previewOn;
+      };
 
-			$scope.scoring = function() {
-				ScoringHandler.scoring($scope.item.components, $scope.item.xhtml,
-					function() {
-						saveComponents();
-					});
-			};
+      $scope.scoring = function() {
+        ScoringHandler.scoring($scope.item.components, $scope.item.xhtml,
+          function() {
+            saveComponents();
+          });
+      };
 
-			$scope.$on('edit-node', function($event, id, model, config) {
-				ComponentPopups.launch($scope, id, model, config);
-			});
+      $scope.$on('edit-node', function($event, id, model, config) {
+        ComponentPopups.launch($scope, id, model, config);
+      });
 
-			$scope.imageService = ComponentImageService;
-			$scope.overrideFeatures = EditorConfig.overrideFeatures;
-			$scope.extraFeatures = EditorConfig.extraFeatures;
+      $scope.imageService = ComponentImageService;
+      $scope.overrideFeatures = EditorConfig.overrideFeatures;
+      $scope.extraFeatures = EditorConfig.extraFeatures;
 
 
-			// Dropdowns in wiggi-wiz toolbar don't trigger when bootstrap is imported?
-			$timeout(function() {
-				$('.wiggi-wiz-toolbar button', $element).dropdown();
-			});
+      // Dropdowns in wiggi-wiz toolbar don't trigger when bootstrap is imported?
+      $timeout(function() {
+        $('.wiggi-wiz-toolbar button', $element).dropdown();
+      });
 
-			function deprecated() {
-				throw new Error('deprecated');
-			}
+      function deprecated() {
+        throw new Error('deprecated');
+      }
 
-			$scope.getUploadUrl = deprecated;
-			$scope.selectFile = deprecated;
+      $scope.getUploadUrl = deprecated;
+      $scope.selectFile = deprecated;
 
-			$scope.$on('fileSizeGreaterThanMax', EditorConfig.onFileSizeGreaterThanMax);
+      $scope.$on('fileSizeGreaterThanMax', EditorConfig.onFileSizeGreaterThanMax);
 
-			$scope.$on('registerComponent', function(event, id, componentBridge,
-				componentElement) {
-				logger.debug('registerComponent ', id);
-				ComponentData.registerComponent(id, componentBridge, componentElement);
-			});
+      $scope.$on('registerComponent', function(event, id, componentBridge,
+        componentElement) {
+        logger.debug('registerComponent ', id);
+        ComponentData.registerComponent(id, componentBridge, componentElement);
+      });
 
-			$scope.$on('registerPlaceholder', function(event, id, placeholder) {
-				logger.debug('registerPlaceholder ', id);
-				ComponentData.registerPlaceholder(id, placeholder);
-			});
+      $scope.$on('registerPlaceholder', function(event, id, placeholder) {
+        logger.debug('registerPlaceholder ', id);
+        ComponentData.registerPlaceholder(id, placeholder);
+      });
 
-			$scope.$on('registerConfigPanel', function(a, id, component) {
-				logger.debug('registerConfigPanel', id);
-				configPanels[id] = component;
-				component.setModel($scope.item.components[id]);
-				if (_.isFunction(component.setProfile)) {
-					component.setProfile($scope.item.profile);
-				}
-			});
+      $scope.$on('registerConfigPanel', function(a, id, component) {
+        logger.debug('registerConfigPanel', id);
+        configPanels[id] = component;
+        component.setModel($scope.item.components[id]);
+        if (_.isFunction(component.setProfile)) {
+          component.setProfile($scope.item.profile);
+        }
+      });
 
-			$scope.onItemSaved = function() {};
+      $scope.onItemSaved = function() {};
 
-			function saveComponents() {
-				logger.debug('[saveComponents]');
-				ItemService.saveComponents(
-					$scope.serialize($scope.item.components),
-					$scope.onItemSaved,
-					$scope.onItemSaveError);
-			}
+      function saveComponents() {
+        logger.debug('[saveComponents]');
+        ItemService.saveComponents(
+          $scope.serialize($scope.item.components),
+          $scope.onItemSaved,
+          $scope.onItemSaveError);
+      }
 
-			$scope.getWiggiWizElement = function() {
-				return angular.element('.wiggi-wiz', $element);
-			};
+      $scope.getWiggiWizElement = function() {
+        return angular.element('.wiggi-wiz', $element);
+      };
 
-			$scope.$on('itemAdded', function(event, $node) {
-				// This ends up in some weird race condition if we don't wrap it in a $timeout
-				$timeout(function() {
-					$scope.getWiggiWizElement().scope().focusCaretAtEnd();
-				});
-			});
+      $scope.$on('itemAdded', function(event, $node) {
+        // This ends up in some weird race condition if we don't wrap it in a $timeout
+        $timeout(function() {
+          $scope.getWiggiWizElement().scope().focusCaretAtEnd();
+        });
+      });
 
-			$scope.serialize = function(comps) {
-				if (!configPanels) {
-					return comps;
-				}
+      $scope.serialize = function(comps) {
+        if (!configPanels) {
+          return comps;
+        }
 
-				var newModel = _.cloneDeep(comps);
+        var newModel = _.cloneDeep(comps);
 
-				function storeComponentModelInNewModel(value, key) {
-					var configPanel = configPanels[key];
-					if (configPanel && configPanel.getModel) {
-						newModel[key] = configPanel.getModel();
-					}
-				}
+        function storeComponentModelInNewModel(value, key) {
+          var configPanel = configPanels[key];
+          if (configPanel && configPanel.getModel) {
+            newModel[key] = configPanel.getModel();
+          }
+        }
 
-				_.each(comps, storeComponentModelInNewModel);
+        _.each(comps, storeComponentModelInNewModel);
 
-				return newModel;
-			};
+        return newModel;
+      };
 
-			$scope.$watch('item.components', debounce(function(newComps, oldComps) {
-				if (_.isEqual(newComps, oldComps)) {
-					logger.debug('they are the same - ignore...');
-					return;
-				}
-				saveComponents();
-			}), true);
+      $scope.$watch('item.components', debounce(function(newComps, oldComps) {
+        if (_.isEqual(newComps, oldComps)) {
+          logger.debug('they are the same - ignore...');
+          return;
+        }
+        saveComponents();
+      }), true);
 
-			$scope.$watch('item.xhtml', debounce(function(oldValue, newValue) {
-				logger.debug('old', oldValue);
-				if (oldValue !== newValue) {
-					ItemService.saveXhtml($scope.item.xhtml);
-				}
-			}));
+      $scope.$watch('item.xhtml', debounce(function(oldValue, newValue) {
+        logger.debug('old', oldValue);
+        if (oldValue !== newValue) {
+          ItemService.saveXhtml($scope.item.xhtml);
+        }
+      }));
 
-			$scope.$watch('item.summaryFeedback', debounce(function(oldValue, newValue) {
-				logger.debug('old', oldValue);
-				if (oldValue !== newValue) {
-					ItemService.saveSummaryFeedback($scope.item.summaryFeedback);
-				}
-			}));
+      $scope.$watch('item.summaryFeedback', debounce(function(oldValue, newValue) {
+        logger.debug('old', oldValue);
+        if (oldValue !== newValue) {
+          ItemService.saveSummaryFeedback($scope.item.summaryFeedback);
+        }
+      }));
 
-			function debounce(fn) {
-				return _.debounce(fn, 500, {
-					trailing: true,
-					leading: false
-				});
-			}
+      function debounce(fn) {
+        return _.debounce(fn, 500, {
+          trailing: true,
+          leading: false
+        });
+      }
 
-			ItemService.load(function(item) {
-				$scope.item = item;
-				ComponentData.setModel(item.components);
-				MathJaxService.parseDomForMath(100);
-			});
+      ItemService.load(function(item) {
+        $scope.item = item;
+        ComponentData.setModel(item.components);
+        MathJaxService.parseDomForMath(100);
+      });
 
-		}
-	]);
+    }
+  ]);
--- modules/container-client/src/js/editor/controllers/root.js
+++ modules/container-client/src/js/editor/controllers/root.js
@@ -1,129 +1,129 @@
 angular.module('corespring-editor.controllers')
-	.controller('Root', [
-		'$scope',
-		'$state',
-		'ComponentRegister',
-		'ConfigurationService',
-		'ItemService',
-		'LogFactory',
-		'Msgr',
-		'WiggiDialogLauncher',
-		'EditorDialogTemplate',
-		'WIGGI_EVENTS',
-		function(
-			$scope,
-			$state,
-			ComponentRegister,
-			ConfigurationService,
-			ItemService,
-			LogFactory,
-			Msgr,
-			WiggiDialogLauncher,
-			EditorDialogTemplate,
-			WIGGI_EVENTS) {
+  .controller('Root', [
+    '$scope',
+    '$state',
+    'ComponentRegister',
+    'ConfigurationService',
+    'ItemService',
+    'LogFactory',
+    'Msgr',
+    'WiggiDialogLauncher',
+    'EditorDialogTemplate',
+    'WIGGI_EVENTS',
+    function(
+      $scope,
+      $state,
+      ComponentRegister,
+      ConfigurationService,
+      ItemService,
+      LogFactory,
+      Msgr,
+      WiggiDialogLauncher,
+      EditorDialogTemplate,
+      WIGGI_EVENTS) {
 
-			"use strict";
+      "use strict";
 
-			var $log = LogFactory.getLogger('RootController');
+      var $log = LogFactory.getLogger('RootController');
 
-			function preprocessComponents(item) {
-				_.each(item.components, function(c, key) {
-					var serverLogic = corespring.server.logic(c.componentType);
-					if (serverLogic.preprocess) {
-						//TODO: This is part of a larger task to add preprocess to the container
-						//@see: https://thesib.atlassian.net/browse/CA-842
-						item.components[key] = serverLogic.preprocess(c);
-					}
-				});
-			}
+      function preprocessComponents(item) {
+        _.each(item.components, function(c, key) {
+          var serverLogic = corespring.server.logic(c.componentType);
+          if (serverLogic.preprocess) {
+            //TODO: This is part of a larger task to add preprocess to the container
+            //@see: https://thesib.atlassian.net/browse/CA-842
+            item.components[key] = serverLogic.preprocess(c);
+          }
+        });
+      }
 
-			function findLastId(item) {
-				var max = 0;
-				$('<div>' + item.xhtml + '</div>').find('[id]').each(function(idx,
-					element) {
-					var id = Number($(element).attr('id'));
-					if (id > max) {
-						max = id;
-					}
-				});
-				return max;
-			}
+      function findLastId(item) {
+        var max = 0;
+        $('<div>' + item.xhtml + '</div>').find('[id]').each(function(idx,
+          element) {
+          var id = Number($(element).attr('id'));
+          if (id > max) {
+            max = id;
+          }
+        });
+        return max;
+      }
 
-			$scope.$on('deleteSupportingMaterial', function(event, data) {
+      $scope.$on('deleteSupportingMaterial', function(event, data) {
 
-				function showFirstItem() {
-					$state.transitionTo('supporting-materials', {
-						index: 0
-					}, {
-						reload: true
-					});
-				}
+        function showFirstItem() {
+          $state.transitionTo('supporting-materials', {
+            index: 0
+          }, {
+            reload: true
+          });
+        }
 
-				function deleteSupportingMaterial(index) {
-					$scope.item.supportingMaterials.splice(index, 1);
+        function deleteSupportingMaterial(index) {
+          $scope.item.supportingMaterials.splice(index, 1);
 
-					ItemService.saveSupportingMaterials($scope.item.supportingMaterials,
-						function() {},
-						$scope.onSaveError, $scope.itemId
-					);
-				}
+          ItemService.saveSupportingMaterials($scope.item.supportingMaterials,
+            function() {},
+            $scope.onSaveError, $scope.itemId
+          );
+        }
 
-				var confirmationMessage = [
-					"You are about to delete this file.",
-					"Are you sure you want to do this?"
-				].join('\n');
+        var confirmationMessage = [
+          "You are about to delete this file.",
+          "Are you sure you want to do this?"
+        ].join('\n');
 
-				if (window.confirm(confirmationMessage)) {
-					showFirstItem();
-					deleteSupportingMaterial(data.index);
-				}
-			});
+        if (window.confirm(confirmationMessage)) {
+          showFirstItem();
+          deleteSupportingMaterial(data.index);
+        }
+      });
 
-			function onLaunchDialog($event, data, title, body, callback, scopeProps, options) {
-				var dialog = new WiggiDialogLauncher($event.targetScope);
-				var header = options.omitHeader ? '' : null;
-				var footer = options.omitFooter ? '' : null;
-				var content = EditorDialogTemplate.generate(title, body, header, footer);
-				dialog.launch(data, content, callback, scopeProps, options);
-			}
+      function onLaunchDialog($event, data, title, body, callback, scopeProps, options) {
+        var dialog = new WiggiDialogLauncher($event.targetScope);
+        var header = options.omitHeader ? '' : null;
+        var footer = options.omitFooter ? '' : null;
+        var content = EditorDialogTemplate.generate(title, body, header, footer);
+        dialog.launch(data, content, callback, scopeProps, options);
+      }
 
-			$scope.$on(WIGGI_EVENTS.LAUNCH_DIALOG, onLaunchDialog);
+      $scope.$on(WIGGI_EVENTS.LAUNCH_DIALOG, onLaunchDialog);
 
-			$scope.onItemLoadSuccess = function(item) {
-				$scope.item = item;
-				preprocessComponents(item);
-				$scope.lastId = findLastId(item);
-				$scope.$broadcast('itemLoaded', item);
-			};
+      $scope.onItemLoadSuccess = function(item) {
+        $scope.item = item;
+        preprocessComponents(item);
+        $scope.lastId = findLastId(item);
+        $scope.$broadcast('itemLoaded', item);
+      };
 
-			$scope.onItemLoadError = function(err) {
-				$log.error('error loading', err);
-			};
+      $scope.onItemLoadError = function(err) {
+        $log.error('error loading', err);
+      };
 
-			ItemService.load($scope.onItemLoadSuccess, $scope.onItemLoadError);
+      ItemService.load($scope.onItemLoadSuccess, $scope.onItemLoadError);
 
-			//----------------------------------------------------------------
-			// startup
-			//----------------------------------------------------------------
+      //----------------------------------------------------------------
+      // startup
+      //----------------------------------------------------------------
 
-			function isInIframe() {
-				/** note use != to support ie8 instead of !== */
-				return top != window; // jshint ignore:line
-			}
+      function isInIframe() {
+        /** note use != to support ie8 instead of !== */
+        return top != window; // jshint ignore:line
+      }
 
-			if (isInIframe()) {
-				Msgr.on('initialise', function(data) {
-					$log.log('on initialise', data);
-					ConfigurationService.setConfig(data);
-					Msgr.send('rendered');
-				});
+      if (isInIframe()) {
+        Msgr.on('initialise', function(data) {
+          $log.log('on initialise', data);
+          ConfigurationService.setConfig(data);
+          Msgr.send('rendered');
+        });
 
-				//send msg "ready" to instance
-				//this will result in msg "initialise" being sent back to us
-				$log.log('sending ready');
-				Msgr.send('ready');
-			} else {
-				ConfigurationService.setConfig({});
-			}
-		}
-	]);
+        //send msg "ready" to instance
+        //this will result in msg "initialise" being sent back to us
+        $log.log('sending ready');
+        Msgr.send('ready');
+      } else {
+        ConfigurationService.setConfig({});
+      }
+    }
+  ]);
