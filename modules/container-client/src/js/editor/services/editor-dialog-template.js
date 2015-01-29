4f38d6c7d24778b3cb797db43ac567327d701c4a
edeustace <ed.eustace@gmail.com>
2015-01-29 15:23:18 +0000

    fix formatting

--- modules/container-client/src/js/editor/services/editor-dialog-template.js
+++ modules/container-client/src/js/editor/services/editor-dialog-template.js
@@ -1,46 +1,46 @@
 angular.module('corespring-editor.services')
-	.service('EditorDialogTemplate', [function() {
+  .service('EditorDialogTemplate', [function() {
 
-		function EditorDialogTemplate() {
+    function EditorDialogTemplate() {
 
-			this.header = function(title) {
-				return [
-					'<div class="modal-header">',
-					'  <button class="close" type="button" ng-click="cancel()">',
-					'    <span>&times;</span>',
-					'    <span class="sr-only">Close</span>',
-					'  </button>',
-					'  <h4 class="modal-title">' + title + '</h4>',
-					'</div>'
-				].join('\n');
-			};
+      this.header = function(title) {
+        return [
+          '<div class="modal-header">',
+          '  <button class="close" type="button" ng-click="cancel()">',
+          '    <span>&times;</span>',
+          '    <span class="sr-only">Close</span>',
+          '  </button>',
+          '  <h4 class="modal-title">' + title + '</h4>',
+          '</div>'
+        ].join('\n');
+      };
 
-			this.footer = function() {
-				return [
-					'<div class="modal-footer right">',
-					' <button class="btn btn-default" type="button" ng-click="ok(data)">Done</button>',
-					'</div>'
-				].join('\n');
-			};
+      this.footer = function() {
+        return [
+          '<div class="modal-footer right">',
+          ' <button class="btn btn-default" type="button" ng-click="ok(data)">Done</button>',
+          '</div>'
+        ].join('\n');
+      };
 
-			function isNullOrUndefined(v){
-				return _.isNull(v) || _.isUndefined(v);
-			} 
-			
-			this.generate = function(title, content, header, footer) {
+      function isNullOrUndefined(v){
+        return _.isNull(v) || _.isUndefined(v);
+      } 
+      
+      this.generate = function(title, content, header, footer) {
 
-				header = isNullOrUndefined(header) ? this.header(title) : header;
-				footer = isNullOrUndefined(footer) ? this.footer() : footer;
+        header = isNullOrUndefined(header) ? this.header(title) : header;
+        footer = isNullOrUndefined(footer) ? this.footer() : footer;
 
-				return [
-					header,
-					'<div class="modal-body">',
-					content,
-					'</div>',
-					footer
-				].join('\n');
-			};
-		}
+        return [
+          header,
+          '<div class="modal-body">',
+          content,
+          '</div>',
+          footer
+        ].join('\n');
+      };
+    }
 
-		return new EditorDialogTemplate();
-	}]);
+    return new EditorDialogTemplate();
+  }]);
