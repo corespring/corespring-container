d97ad41eefad274e4e3cfac88606d9e8727261ef
Attila Miklosi <attila.miklosi@gmail.com>
2015-02-12 13:36:04 +0100

    fixed placement ordering issues after the review

--- modules/container-client/test/js/common/directives/ng-bind-html-unsafe-test.js
+++ modules/container-client/test/js/common/directives/ng-bind-html-unsafe-test.js
@@ -2,31 +2,40 @@ describe('ngBindHtmlUnsafe', function() {
 
   var scope, element;
   var html = "This is some <strong>unsafe</strong> html. Spooky.";
+  var mathService = {parseDomForMath: function() {}};
 
   beforeEach(angular.mock.module('corespring-common.directives'));
 
+  beforeEach(module(function($provide) {
+    $provide.value('MathJaxService', mathService);
+  }));
+
   beforeEach(inject(function($rootScope, $compile) {
     scope = $rootScope.$new();
-    scope.html = html;
     element = angular.element('<div ng-bind-html-unsafe="html"></div>');
     $compile(element)(scope);
-    scope = element.isolateScope();
-    scope.$apply();
+    scope.html = html;
+    scope.$digest();
   }));
 
   it('should add html as child', function() {
     expect($('.ng-binding', element).html()).toEqual(html);
   });
 
-  it('should update html on value update', function() {
+  describe('value update', function() {
     var newHtml = "this is some <em>new</em> unsafe html!";
 
-    beforeEach(function() {
+    it('should add new html as child', function() {
       scope.html = newHtml;
+      scope.$apply();
+      expect($('.ng-binding', element).html()).toEqual(newHtml);
     });
 
-    it('should add new html as child', function() {
-      expect($('.ng-binding', element).html()).toEqual(newHtml);
+    it('should parse node for MathJax', function() {
+      spyOn(mathService, 'parseDomForMath');
+      scope.html = newHtml;
+      scope.$apply();
+      expect(mathService.parseDomForMath).toHaveBeenCalled();
     });
 
   });
