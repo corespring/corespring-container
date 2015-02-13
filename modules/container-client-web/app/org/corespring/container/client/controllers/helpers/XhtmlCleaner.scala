8391148a8316298e62885b92f325ac56bccc1fe1
benburton <benjamin.joseph.burton@gmail.com>
2015-02-13 13:22:18 -0500

    Revert "Merge pull request #299 from corespring/feature/PE-176-fix-missing-spaces"
    This reverts commit 01cf65cf970b4fbbe295dbec292219bab27434c9, reversing
    changes made to a299fda66829a9f59c00168a7d8806051daa420a.

---
+++ modules/container-client-web/app/org/corespring/container/client/controllers/helpers/XhtmlCleaner.scala
@@ -0,0 +1,22 @@
+package org.corespring.container.client.controllers.helpers
+
+import org.htmlcleaner.{ CompactXmlSerializer, TagNode, HtmlCleaner }
+
+object XhtmlCleaner {
+  val cleaner = {
+    val cleaner: HtmlCleaner = new HtmlCleaner()
+    cleaner.getProperties.setUseEmptyElementTags(false)
+    cleaner.getProperties.setOmitXmlDeclaration(true)
+    cleaner.getProperties.setOmitHtmlEnvelope(true)
+    cleaner
+  }
+}
+
+trait XhtmlCleaner {
+
+  def cleanXhtml(s: String): String = {
+    val n: TagNode = XhtmlCleaner.cleaner.clean(s)
+    val serializer = new CompactXmlSerializer(XhtmlCleaner.cleaner.getProperties)
+    serializer.getAsString(n)
+  }
+}
--- modules/container-client-web/app/org/corespring/container/client/controllers/helpers/XhtmlProcessor.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/helpers/XhtmlProcessor.scala
@@ -1,50 +1,51 @@
 package org.corespring.container.client.controllers.helpers
 
-import org.xml.sax.SAXParseException
-
-import scala.xml.transform.{RewriteRule, RuleTransformer}
-import scala.xml._
+import org.htmlcleaner._
+import scala.util.matching.Regex
 
 trait XhtmlProcessor {
 
   /**
    * Converts xhtml by moving custom tag names to attributes instead
-   * TODO: This should *NOT* use regular expressions. Should parse DOM using scala.xml._
    */
-  def tagNamesToAttributes(xhtml: String): String = {
-    val replacements: Seq[String => String] = Seq(
-      s => "(?<=<\\/)(corespring-.*?)(?=>)".r.replaceAllIn(s, { m => "div"}),
-      s => "(?<=<)(corespring-.*?)(?=\\s|>)".r.replaceAllIn(s, { m => s"""div ${m.group(1)}="${m.group(1)}""""})
-    )
-    replacements.foldLeft(xhtml)((replacement, acc) => acc(replacement))
+  def tagNamesToAttributes(xhtml: String): Option[String] = {
+
+    val substitutedClosingTag = "(?<=<\\/)(corespring-.*?)(?=>)".r
+      .replaceAllIn(xhtml,{m => "div"})
+
+    val substitutedOpeningTag = "(?<=<)(corespring-.*?)(?=\\s|>)".r
+      .replaceAllIn(substitutedClosingTag,{m => s"""div ${m.group(1)}="${m.group(1)}""""})
+
+    val cleaner: HtmlCleaner = new HtmlCleaner()
+    val transformations: CleanerTransformations = new CleanerTransformations()
+
+    val pToDiv = new TagTransformation("p", "div", true)
+    pToDiv.addAttributeTransformation("class", "para ${class}")
+    transformations.addTransformation(pToDiv)
+    cleaner.getProperties.setCleanerTransformations(transformations)
+
+    cleaner.getProperties.setUseEmptyElementTags(false)
+    cleaner.getProperties.setOmitXmlDeclaration(true)
+    cleaner.getProperties.setOmitHtmlEnvelope(true)
+
+    val n: TagNode = cleaner.clean(substitutedOpeningTag)
+    val serializer = new CompactXmlSerializer(cleaner.getProperties)
+    val cleanHtml = serializer.getAsString(n)
+
+    //the cleaner creates class="para " (with an extra blank) when the p does not have class
+    //the regexp below removes that blank
+    Some("""class="para """".r.replaceAllIn(cleanHtml, {m => """class="para""""}))
   }
 
-  def translateParagraphsToDivs(xhtml: String) = new RuleTransformer(new RewriteRule {
-    override def transform(n: Node) = n match {
-      case n: Elem if n.label == "p" => n.copy(label = "div") %
-        Attribute(null, "class",
-          s"para${n.attribute("class").map(c => if (c.text.isEmpty) "" else s" ${c.text}").getOrElse("")}",
-          Null)
-      case n => n
-    }
-  }).transform(stringToNodes(xhtml)).mkString
-
-  private def stringToNodes(xhtml: String): NodeSeq = {
-    val wrapper = "div"
-    XML.loadString(s"<$wrapper>$xhtml</$wrapper>").child
+  def toWellFormedXhtml(html: String): String = {
+    val cleaner: HtmlCleaner = new HtmlCleaner()
+    cleaner.getProperties.setUseEmptyElementTags(false)
+    cleaner.getProperties.setOmitXmlDeclaration(true)
+    cleaner.getProperties.setOmitHtmlEnvelope(false)
+    val n: TagNode = cleaner.clean(html)
+    val serializer = new CompactXmlSerializer(cleaner.getProperties)
+    serializer.getAsString(n)
   }
-
-}
-
-object XhtmlProcessor extends XhtmlProcessor {
-
-  implicit class StringWithProcessor(string: String) {
-    def tagNamesToAttributes = XhtmlProcessor.super.tagNamesToAttributes(string)
-    def translateParagraphsToDivs = XhtmlProcessor.super.translateParagraphsToDivs(string)
-
-    def toWellFormedXhtml = string.tagNamesToAttributes.translateParagraphsToDivs
-  }
-
 }
 
 
