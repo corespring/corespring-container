6d3f605161f4c28836239ac0aeb98e11bc4b9285
rbokel <rbokel.github@qlod.org>
2015-04-16 19:54:37 +0200

    AC-102 encode file before loading asset

--- modules/container-client-web/app/org/corespring/container/client/controllers/Assets.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/Assets.scala
@@ -4,6 +4,7 @@ import org.corespring.container.client.HasContext
 import org.corespring.container.client.hooks.AssetHooks
 import org.corespring.container.logging.ContainerLogger
 import play.api.mvc._
+import play.utils.UriEncoding
 
 import scala.concurrent.Future
 
@@ -22,7 +23,6 @@ trait Assets extends Controller with HasContext {
   private def at(id: String, file: String, notFoundLocally: String => SimpleResult) = Action.async {
     request =>
       val result: Future[SimpleResult] = controllers.Assets.at(resourcePath, file)(request)
-
       result.map { r =>
         r match {
           case s: SimpleResult if Seq(OK, NOT_MODIFIED).contains(s.header.status) => s
@@ -34,39 +34,46 @@ trait Assets extends Controller with HasContext {
       }
   }
 
+  private def encode(s:String) = {
+    UriEncoding.encodePathSegment(s, "utf-8")
+  }
+
   def session(sessionId: String, file: String) = Action.async {
     request =>
-      at(sessionId, file, (s) => {
-        logger.trace(s"[session] sessionId: $sessionId -> $file")
+      val encodedFile = encode(file)
+      at(sessionId, encodedFile, (s) => {
+        logger.trace(s"[session] sessionId: $sessionId -> $encodedFile")
         getItemId(s).map {
           itemId =>
-            logger.trace(s"[session] sessionId: $sessionId, itemId: $itemId -> $file")
-            loadAsset(itemId, "data", file)(request)
+            logger.trace(s"[session] sessionId: $sessionId, itemId: $itemId -> $encodedFile")
+            loadAsset(itemId, "data", encodedFile)(request)
         }.getOrElse(NotFound(s"Can't find session id: $sessionId, path: ${request.path}"))
       })(request)
   }
 
   def item(itemId: String, file: String) = Action.async {
     request =>
-      at(itemId, file, (i: String) => {
-        loadAsset(itemId, "data", file)(request)
+      val encodedFile = encode(file)
+      at(itemId, encodedFile, (i: String) => {
+        loadAsset(itemId, "data", encodedFile)(request)
       })(request)
   }
 
   def supportingMaterial(itemId: String, name: String, file: String) = Action.async {
     request =>
-      at(itemId, s"$name/$file", (i: String) => {
-        loadAsset(itemId, name, file)(request)
+      val encodedFile = encode(file)
+      at(itemId, s"$name/$encodedFile", (i: String) => {
+        loadAsset(itemId, name, encodedFile)(request)
       })(request)
   }
 
-  def upload(id: String, file: String) = hooks.uploadAction(id, file) {
+  def upload(id: String, file: String) = hooks.uploadAction(id, encode(file)) {
     r => Ok("")
   }
 
   def delete(itemId: String, file: String) = Action.async {
     implicit request =>
-      hooks.delete(itemId, file).map { err =>
+      hooks.delete(itemId, encode(file)).map { err =>
         err match {
           case None => Ok
           case Some((code, msg)) => Status(code)(msg)
