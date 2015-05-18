bf8f8ef269184d4552045201620381627acc51f0
edeustace <ed.eustace@gmail.com>
2015-05-18 17:46:02 +0100

    ie8 support

--- modules/container-client/src/js/player-launcher/player.js
+++ modules/container-client/src/js/player-launcher/player.js
@@ -1,4 +1,32 @@
 exports.define = function(isSecure) {
+
+
+  /** IE8 Support */
+  /* jshint ignore:start */
+  if (!Array.prototype.indexOf)
+  {
+    Array.prototype.indexOf = function(elt /*, from*/)
+    {
+      var len = this.length >>> 0;
+
+      var from = Number(arguments[1]) || 0;
+      from = (from < 0)
+           ? Math.ceil(from)
+           : Math.floor(from);
+      if (from < 0)
+        from += len;
+
+      for (; from < len; from++)
+      {
+        if (from in this &&
+            this[from] === elt)
+          return from;
+      }
+      return -1;
+    };
+  }
+  /* jshint ignore:end */
+
   var PlayerDefinition = function(element, options, errorCallback) {
 
     var Launcher = require('client-launcher');
