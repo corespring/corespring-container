69c9a8c8c8ffee09b945c979170fb2005826e496
Ed Eustace <ed.eustace@gmail.com>
2015-10-26 10:19:05 +0000

    move editor and player to prevent routes conflicts

---
+++ modules/container-client-web/app/org/corespring/container/client/controllers/launcher/editor/EditorLauncher.scala
@@ -0,0 +1,15 @@
+package org.corespring.container.client.controllers.launcher.editor
+
+import org.corespring.container.client.controllers.launcher.Launcher
+import play.api.mvc.Action
+
+trait EditorLauncher extends Launcher {
+
+  def editorJs = Action.async { implicit request =>
+    hooks.editorJs.map { implicit js =>
+      val config = mkPaths(Paths.editors)
+      make(Seq(draftEditorNameAndSrc, itemEditorNameAndSrc), config, Definitions.editors)
+    }
+  }
+
+}
