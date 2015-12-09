45483ddd1c1b72b7e66d2e32723c611c2cb31e78
rbokel <rbokel.github@qlod.org>
2015-12-09 16:00:59 +0100

    temporarely comment out ben's last commit

--- modules/component-model/src/main/scala/org/corespring/container/components/model/packaging/models.scala
+++ modules/component-model/src/main/scala/org/corespring/container/components/model/packaging/models.scala
@@ -4,17 +4,22 @@ import play.api.libs.json.{ Json, JsObject, JsValue }
 
 case class ClientSideDependency(name: String, files: Seq[String], angularModule: Option[String]) {
 
+  /* todo ben
   def jsFiles = files.filter(_.endsWith(".js"))
   def cssFiles = files.filter(_.endsWith(".css"))
+  */
 
 }
 
 object ClientSideDependency {
 
   def apply(name: String, json: JsValue): ClientSideDependency = {
+    /* todo ben
     val files = Seq("files", "file", "src", "js", "css").map(key =>
       (json \ key).asOpt[String] ++ (json \ key).asOpt[Seq[String]].getOrElse(Seq.empty)
     ).flatten
+    */
+    val files = (json \ "file").asOpt[String].toSeq
     val ngModule = (json \ "angular-module").asOpt[String]
     ClientSideDependency(name, files, ngModule)
   }
