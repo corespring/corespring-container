583b3ac54524e3fdaf63b4ccd4b65e3b060db962
rbokel <rbokel.github@qlod.org>
2015-12-15 15:01:21 +0100

    PE-420 fix path issue in ItemAssetResolver

--- modules/container-client-web/app/org/corespring/container/client/ItemAssetResolver.scala
+++ modules/container-client-web/app/org/corespring/container/client/ItemAssetResolver.scala
@@ -10,13 +10,6 @@ trait ItemAssetResolver {
   protected def mkPath(itemId: String)(imageSrc: String): String = {
     val plainImageSrc = imageSrc.split('/').last
     val playerRoutes = org.corespring.container.client.controllers.apps.routes.Player
-    val url = playerRoutes.getFileByItemId(itemId, plainImageSrc).url
-    withoutEndingSlash(modulePath) + url
-  }
-
-  protected def modulePath: String = v2Player.Routes.prefix
-
-  protected def withoutEndingSlash(s:String): String = {
-   if(s.endsWith("/")) s.substring(0,s.length-1) else s
+    playerRoutes.getFileByItemId(itemId, plainImageSrc).url
   }
 }
