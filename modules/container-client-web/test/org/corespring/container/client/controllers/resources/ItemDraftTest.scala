848b315d67caeabb8d3596fd90af8a54ca4f6732
rbokel <rbokel.github@qlod.org>
2016-02-11 11:42:13 +0100

    revert tests too

--- modules/container-client-web/test/org/corespring/container/client/controllers/resources/ItemDraftTest.scala
+++ modules/container-client-web/test/org/corespring/container/client/controllers/resources/ItemDraftTest.scala
@@ -1,6 +1,5 @@
 package org.corespring.container.client.controllers.resources
 
-import org.corespring.container.client.controllers.helpers.PlayerXhtml
 import org.corespring.container.client.controllers.resources.ItemDraft.Errors
 import org.corespring.container.client.hooks.Hooks.StatusMessage
 import org.corespring.container.client.hooks._
@@ -49,6 +48,7 @@ class ItemDraftTest extends Specification with Mockito {
             m.load(anyString)(any[RequestHeader]) returns Future(Right(loadResult))
             m
           }
+
         }
       }
 
@@ -56,12 +56,13 @@ class ItemDraftTest extends Specification with Mockito {
         status(draft.load("x")(FakeRequest("", ""))) === OK
       }
 
-      val badJson = Json.obj(
+      val badJson = Json.obj("_id" ->
+        Json.obj("$oid" -> "1"),
         "xhtml" -> "<p>a</p>")
 
       "prep the json" in new load(loadResult = badJson) {
         val json = contentAsJson(draft.load("x")(FakeRequest("", "")))
-        (json \ "itemId").as[String] === "x"
+        (json \ "itemId").as[String] === "1"
         (json \ "xhtml").as[String] === """<div class="para">a</div>"""
       }
     }
