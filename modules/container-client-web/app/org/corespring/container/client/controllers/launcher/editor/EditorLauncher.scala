58f6d7c68439b362a9a460708710eb0753140ca7
Ed Eustace <ed.eustace@gmail.com>
2016-02-18 18:14:21 +0000

    inject js builder

--- modules/container-client-web/app/org/corespring/container/client/controllers/launcher/editor/EditorLauncher.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/launcher/editor/EditorLauncher.scala
@@ -1,14 +1,15 @@
 package org.corespring.container.client.controllers.launcher.editor
 
-import org.corespring.container.client.controllers.launcher.{ItemEditors, Launcher}
+import org.corespring.container.client.controllers.launcher.{ JsBuilder, ItemEditors, Launcher }
 import play.api.mvc.Action
 
 trait EditorLauncher extends Launcher {
 
+  def builder: JsBuilder
+
   def editorJs = Action.async { implicit request =>
     hooks.editorJs.map { implicit js =>
-      ItemEditors(playerConfig, request).result
+      ItemEditors(playerConfig, request, builder).result
     }
   }
-
 }
--- modules/container-client-web/app/org/corespring/container/client/controllers/launcher/player/PlayerLauncher.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/launcher/player/PlayerLauncher.scala
@@ -5,28 +5,30 @@ import play.api.mvc._
 
 trait PlayerLauncher extends Launcher {
 
+  def builder: JsBuilder
+
   def componentEditorJs = Action.async { implicit request =>
     hooks.componentEditorJs.map { err =>
-      ComponentEditor(playerConfig, request).result
+      ComponentEditor(playerConfig, request, builder).result
     }
   }
 
   def editorJs = Action.async { implicit request =>
     hooks.editorJs.map { implicit js =>
-      ItemEditors(playerConfig, request).result
+      ItemEditors(playerConfig, request, builder).result
     }
   }
 
   def catalogJs = Action.async { implicit request =>
     hooks.catalogJs.map { implicit js =>
-      Catalog(playerConfig, request).result
+      Catalog(playerConfig, request, builder).result
     }
   }
 
   def playerJs = Action.async { implicit request =>
     hooks.playerJs.map { implicit js =>
       logger.debug(s"playerJs - isSecure=${js.isSecure}, path=${request.path}, queryString=${request.rawQueryString}")
-      Player(playerConfig, request, js).result
+      Player(playerConfig, request, js, builder).result
     }
   }
 
