1dddd5ec10ab5728120ee0322fbae041582e1bcd
Ed Eustace <ed.eustace@gmail.com>
2016-02-19 11:50:28 +0000

    endpoint image test

--- modules/container-client/test/js/component-editor/services/external-endpoint-image-service-test.js
+++ modules/container-client/test/js/component-editor/services/external-endpoint-image-service-test.js
@@ -1,43 +1,101 @@
 describe('external-endpoint-image-service', function(){
 
-
-  var Def;
+  var e = org.corespring.mocks.editor;
+  var def, Def, fileReader, fileUploader, $http;
 
   beforeEach(angular.mock.module('corespring-singleComponentEditor.services'));
 
   beforeEach(module(function($provide) {
-    $provide.value('$http', {});
+    $http = e.$http();
+    $provide.value('$http', $http);
     $provide.value('UPLOAD_ENDPOINT', {url: 'url/:filename'});
   }));
 
-
-  var e = org.corespring.mocks.editor;
-
-  e.withWindowMocks({
-    FileReader: new (e.FileReader())(),
-    'com.ee.RawFileUploader': e['com.ee.RawFileUploader']()
-  }, function(mocks){
-    
-
-    beforeEach(inject(function(ExternalEndpointsImageService) {
-      Def = ExternalEndpointsImageService;
-    }));
-
-    describe('initialization', function(){
-      it('inits', function(){
-        expect(new Def()).not.toBe(undefined);
-      });
-    });
-    
-    describe('addFile', function(){
-      it('inits', function(){
-        var def = new Def();
-        def.addFile('file', jasmine.createSpy('onComplete', 'onProgress'));
-        expect(mocks.FileReader.readAsBinaryString).toHaveBeenCalled();
-      });
-    });
-
+  beforeAll(function(){
+    fileReader = e.FileReader();
+    fileUploader = e['com.ee.RawFileUploader']();
   });
+    
+  beforeEach(inject(function(ExternalEndpointsImageService) {
+    Def = ExternalEndpointsImageService;
+  }));
+      
+  describe('initialization', function(){
+    it('inits', function(){
+      expect(new Def()).not.toBe(undefined);
+    });
+  });
+
+  describe('deleteFile', function(){
+
+    beforeEach(function(){
+      def = new Def();
+      def.deleteFile('url');
+    });
+
+    it('calls $http', function(){
+      expect($http).toHaveBeenCalledWith({
+        method: 'DELETE', url: 'url'
+      });
+    });
+  });
+
+  describe('addFile', e.withWindowMocks(
+    function(){ 
+      return { FileReader: fileReader,
+        'com.ee.RawFileUploader': fileUploader 
+      };
+    },
+    function(getMocks){
+      
+      describe('successfully', function(){
+
+        var m, onComplete;
+        
+        beforeEach(function(){
+          m = getMocks();
+          var uploader = m['com.ee.RawFileUploader']();
+          uploader.beginUpload.and.callFake(function(){
+            uploader.uploadOpts.onUploadComplete('url', 200);
+          });
+          def = new Def();
+          onComplete = jasmine.createSpy('onComplete');
+          def.addFile({name: 'file'}, onComplete, jasmine.createSpy('onProgress'));
+        });
+
+        it('calls reader.readAsBinaryString', function(){
+          expect(m.FileReader().readAsBinaryString).toHaveBeenCalled();
+        });
+        
+        it('calls fileUploader.beginUpload', function(){
+          expect(m['com.ee.RawFileUploader']().beginUpload).toHaveBeenCalled();
+        });
+        
+        it('calls onComplete', function(){
+          expect(onComplete).toHaveBeenCalledWith(null, 'url');
+        });
+      });
+         
+      describe('with error', function(){
+          
+          var m; 
+          beforeEach(function(){
+            m = getMocks();
+            def = new Def();
+            onComplete = jasmine.createSpy('onComplete');
+            var uploader = m['com.ee.RawFileUploader']();
+            uploader.beginUpload.and.callFake(function(){
+              uploader.uploadOpts.onUploadFailed('err');
+            });
+            def.addFile({name: 'file'}, onComplete, jasmine.createSpy('onProgress'));
+          });
+          
+          it('calls onComplete with err', function(){
+            expect(onComplete).toHaveBeenCalledWith('Upload failed for: url/file: err');
+          });
+
+      });
+  }));
 
 
 });
