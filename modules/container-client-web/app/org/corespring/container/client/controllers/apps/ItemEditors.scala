3eacd727b8a017e6425233f3ef22dabcc45a0072
Ed Eustace <ed.eustace@gmail.com>
2016-02-25 11:45:32 +0000

    wip

--- modules/container-client-web/app/org/corespring/container/client/controllers/apps/ItemEditors.scala
+++ modules/container-client-web/app/org/corespring/container/client/controllers/apps/ItemEditors.scala
@@ -1,22 +1,44 @@
 package org.corespring.container.client.controllers.apps
 
 import org.corespring.container.client.controllers.resources
-import org.corespring.container.client.views.models.{MainEndpoints, SupportingMaterialsEndpoints}
-import org.corespring.container.client.views.txt.js.{ComponentEditorServices, EditorServices}
-import play.api.libs.json.{JsArray, JsValue}
-
-
+import org.corespring.container.client.controllers.resources.routes._
+import org.corespring.container.client.views.models.{ MainEndpoints, SupportingMaterialsEndpoints }
+import org.corespring.container.client.views.txt.js.{ ComponentEditorServices, EditorServices }
+import play.api.libs.json.{ JsArray, JsValue }
+import play.api.mvc.Call
 import resources.{ routes => resourceRoutes }
 
-
-trait Endpoints{
-  def main(id:String) : MainEndpoints
-  def supportingMaterials(id:String) :SupportingMaterialsEndpoints
+trait Endpoints {
+  def main(id: String): MainEndpoints
+  def supportingMaterials(id: String): SupportingMaterialsEndpoints
 }
 
-object ItemEditorEndpoints extends Endpoints{
+case class SessionEndpoints(
+  loadItemAndSession: Call,
+  reopen: Call,
+  reset: Call,
+  save: Call,
+  getScore: Call,
+  complete: Call,
+  loadOutcome: Call,
+  loadInstructorData: Call)
 
-  def supportingMaterials(itemId:String) = SupportingMaterialsEndpoints(
+object PlayerEndpoints {
+
+  def session(sessionId: String) = SessionEndpoints(
+    Session.loadItemAndSession(sessionId),
+    Session.reopenSession(sessionId),
+    Session.resetSession(sessionId),
+    Session.saveSession(sessionId),
+    Session.getScore(sessionId),
+    Session.completeSession(sessionId),
+    Session.loadOutcome(sessionId),
+    Session.loadInstructorData(sessionId))
+}
+
+object ItemEditorEndpoints extends Endpoints {
+
+  def supportingMaterials(itemId: String) = SupportingMaterialsEndpoints(
     create = resourceRoutes.Item.createSupportingMaterial(itemId),
     createFromFile = resourceRoutes.Item.createSupportingMaterialFromFile(itemId),
     delete = resourceRoutes.Item.deleteSupportingMaterial(itemId, ":name"),
@@ -25,16 +47,15 @@ object ItemEditorEndpoints extends Endpoints{
     getAsset = resourceRoutes.Item.getAssetFromSupportingMaterial(itemId, ":name", ":filename"),
     updateContent = resourceRoutes.Item.updateSupportingMaterialContent(itemId, ":name", ":filename"))
 
-  def main(itemId:String) = MainEndpoints(
+  def main(itemId: String) = MainEndpoints(
     load = resourceRoutes.Item.load(itemId),
     saveSubset = resourceRoutes.Item.saveSubset(itemId, ":subset"),
-    save = None
-  )
+    save = None)
 }
 
-object DraftEditorEndpoints extends Endpoints{
+object DraftEditorEndpoints extends Endpoints {
 
-  def supportingMaterials(id:String)= SupportingMaterialsEndpoints(
+  def supportingMaterials(id: String) = SupportingMaterialsEndpoints(
     create = resourceRoutes.ItemDraft.createSupportingMaterial(id),
     createFromFile = resourceRoutes.ItemDraft.createSupportingMaterialFromFile(id),
     delete = resourceRoutes.ItemDraft.deleteSupportingMaterial(id, ":name"),
@@ -43,11 +64,10 @@ object DraftEditorEndpoints extends Endpoints{
     getAsset = resourceRoutes.ItemDraft.getAssetFromSupportingMaterial(id, ":name", ":filename"),
     updateContent = resourceRoutes.ItemDraft.updateSupportingMaterialContent(id, ":name", ":filename"))
 
-  def main(id:String) = MainEndpoints(
+  def main(id: String) = MainEndpoints(
     load = resourceRoutes.ItemDraft.load(id),
     saveSubset = resourceRoutes.ItemDraft.saveSubset(id, ":subset"),
-    save = Some(resourceRoutes.ItemDraft.save(id))
-  )
+    save = Some(resourceRoutes.ItemDraft.save(id)))
 }
 
 trait BaseItemEditor extends CoreEditor {
@@ -65,14 +85,14 @@ trait BaseItemEditor extends CoreEditor {
       getAsset = resourceRoutes.Item.getAssetFromSupportingMaterial(itemId, ":name", ":filename"),
       updateContent = resourceRoutes.Item.updateSupportingMaterialContent(itemId, ":name", ":filename"))
     "??"
-//    EditorServices(
-//      s"$context.services",
-//      resourceRoutes.Item.load(itemId),
-//      resourceRoutes.Item.saveSubset(itemId, ":subset"),
-//      None,
-//      smEndpoints,
-//      components,
-//      widgets).toString
+    //    EditorServices(
+    //      s"$context.services",
+    //      resourceRoutes.Item.load(itemId),
+    //      resourceRoutes.Item.saveSubset(itemId, ":subset"),
+    //      None,
+    //      smEndpoints,
+    //      components,
+    //      widgets).toString
   }
 }
 
--- modules/container-client-web/app/org/corespring/container/client/pages/PlayerRenderer.scala
+++ modules/container-client-web/app/org/corespring/container/client/pages/PlayerRenderer.scala
@@ -1,11 +1,14 @@
 package org.corespring.container.client.pages
 
 import org.corespring.container.client.VersionInfo
-import org.corespring.container.client.component.ComponentJson
+import org.corespring.container.client.component.{ ComponentJson, ComponentsScriptBundle }
 import org.corespring.container.client.controllers.apps.PageSourceService
 import org.corespring.container.client.integration.ContainerExecutionContext
 import org.corespring.container.client.pages.engine.JadeEngine
 import org.corespring.container.client.pages.processing.AssetPathProcessor
+import org.corespring.container.client.views.txt.js.{ ComponentEditorServices, PlayerServices }
+import play.api.Logger
+import play.api.libs.json.{ JsArray, Json }
 import play.api.templates.Html
 
 import scala.concurrent.{ ExecutionContext, Future }
@@ -20,10 +23,38 @@ class PlayerRenderer(
 
   implicit def ec: ExecutionContext = containerContext.context
 
-  def render: Future[Html] = Future {
+  private lazy val logger = Logger(classOf[PlayerRenderer])
 
+  def name = "player"
+
+  private object sources {
+    lazy val js = {
+      pageSourceService.loadJs(name)
+    }
+
+    lazy val css = {
+      pageSourceService.loadCss(name)
+    }
+  }
+
+  def render(bundle: ComponentsScriptBundle, prodMode: Boolean): Future[Html] = Future {
+
+    logger.info(s"function=render, bundle=$bundle")
+
+    val css = if (prodMode) Seq(sources.css.dest) else sources.css.src
+    val js = if (prodMode) Seq(sources.js.dest) else sources.js.src
+    val inlineJs = PlayerServices("name", endpoints, queryParams).toString
+    //arr, componentBundle.componentType).toString
+    val processedCss = (css ++ bundle.css).map(assetPathProcessor.process)
+    val processedJs = (sources.js.otherLibs ++ js ++ bundle.js).map(assetPathProcessor.process)
     val params: Map[String, Any] = Map(
-      "appName" -> "player")
+      "appName" -> "player",
+      "js" -> processedJs.toArray,
+      "css" -> processedCss.toArray,
+      "ngModules" -> (sources.js.ngModules ++ bundle.ngModules).map(s => s"'$s'").mkString(","),
+      "ngServiceLogic" -> inlineJs,
+      "options" -> "{}",
+      "versionInfo" -> Json.stringify(versionInfo.json))
 
     jadeEngine.renderJade("player", params)
   }
