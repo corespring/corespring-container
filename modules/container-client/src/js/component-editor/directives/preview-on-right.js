e076afc767652b823a63c9ae31f4487af639ce15
Attila Miklosi <attila.miklosi@gmail.com>
2016-03-03 17:45:40 +0100

    programmatic solution for scrolling the component editor within the iframe

---
+++ modules/container-client/src/js/component-editor/directives/preview-on-right.js
@@ -0,0 +1,17 @@
+angular.module('corespring-singleComponentEditor.directives')
+  .directive('previewOnRight', [
+    function() {
+      return {
+        restrict: 'A',
+        link: function($scope, $elem, $attrs) {
+          var previewWidth = parseInt($attrs.previewWidth || "630");
+          $elem.width(previewWidth + 670);
+          $scope.showPreview = true;
+          $scope.$on('showPreview', function(event, show) {
+            $scope.showPreview = show;
+          });
+        }
+      }
+    }
+  ]
+);
