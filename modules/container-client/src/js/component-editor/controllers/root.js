f296409b62c2279de3fb3518e6fcf2aab529d75d
Ed Eustace <ed.eustace@gmail.com>
2016-03-04 09:59:57 +0000

    rm useless watch

--- modules/container-client/src/js/component-editor/controllers/root.js
+++ modules/container-client/src/js/component-editor/controllers/root.js
@@ -140,11 +140,6 @@ angular.module('corespring-singleComponentEditor.controllers')
         });
       });
 
-      // $scope.$watch('item', function(){
-      //   $scope.$broadcast('client-side-preview.reset-player');
-      // }, true);
-
-
       function onLaunchDialog($event, data, title, body, callback, scopeProps, options) {
         var dialog = new WiggiDialogLauncher($event.targetScope);
         var header = options.omitHeader ? '' : null;
