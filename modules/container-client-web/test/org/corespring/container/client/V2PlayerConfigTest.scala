ef1ab015d84620780b2f07002127e010d778daa9
Ed Eustace <ed.eustace@gmail.com>
2016-03-10 10:40:22 +0000

    remove Configuration out of DefaultIntegration + bump to 2 (breaking change)

--- modules/container-client-web/test/org/corespring/container/client/V2PlayerConfigTest.scala
+++ modules/container-client-web/test/org/corespring/container/client/V2PlayerConfigTest.scala
@@ -1,38 +1,18 @@
 package org.corespring.container.client
 
-import com.typesafe.config.ConfigFactory
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification._
-import play.api.Configuration
 
 class V2PlayerConfigTest extends Specification with Mockito {
 
-  def configString(enabled: Boolean) =
-    s"""{corespring:{v2player:{newrelic:{enabled:$enabled, license-key: key, application-id:id}}}}"""
-
-  class scope(configJson: String) extends Scope {
-
-    val config = ConfigFactory.parseString(configJson)
-    val configuration = new Configuration(config)
-    val playerConfig = V2PlayerConfig(configuration)
-  }
+  class scope extends Scope
 
   "apply" should {
-    "return a new config" in new scope(configString(true)) {
-      playerConfig.newRelicRumConfig must_== Some(NewRelicRumConfig("key", "id"))
+
+    "set useNewRelic to false, new relic config is None" in new scope {
+      V2PlayerConfig(None, None).useNewRelic === false
     }
 
-    "not return a config if enabled is false" in new scope(configString(false)) {
-      playerConfig.newRelicRumConfig === None
-    }
-
-    "set useNewRelic to false, if enabled is false" in new scope(configString(false)) {
-      playerConfig.useNewRelic === false
-    }
-
-    "be converted to proper json config object" in new scope(configString(true)) {
-      playerConfig.newRelicRumConfig.map(_.json) === Some(NewRelicRumConfig("key", "id").json)
-    }
   }
 }
