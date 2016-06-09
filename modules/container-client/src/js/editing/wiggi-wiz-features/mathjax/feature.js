349d41b3ca150bf29c31da83f3bbca7791f5b1ba
Attila Miklosi <attila.miklosi@gmail.com>
2016-06-09 15:41:19 +0200

    math feature: fixed issues due to html characters in the latex/mathml

--- modules/container-client/src/js/editing/wiggi-wiz-features/mathjax/feature.js
+++ modules/container-client/src/js/editing/wiggi-wiz-features/mathjax/feature.js
@@ -15,14 +15,17 @@ angular.module('corespring-editing.wiggi-wiz-features.mathjax').factory('WiggiMa
       this.compile = true;
       this.draggable = true;
       this.initialise = function($node, replaceWith) {
-        var content = $node.html() || '';
+        var preContent = $node.html();
+        var content = preContent;
+        if (!/<math.*?>/gi.test(preContent)) {
+          content = $node.text();
+        }
         var isNew = $node[0].outerHTML.indexOf('mathinput-holder-init') >= 0;
-        content = content.replace(/\\\(/gi,'').replace(/\\\)/gi, '').replace(/\\/gi,'\\\\').replace(/"/g,'\\\\"');
-        content = content.replace(/=\\\\"(.*?)\\\\"/gi,'=\\\'$1\\\'');
-
+        content = content.replace(/\\\(/gi,'').replace(/\\\)/gi, '')
+        var encodedContent = btoa(content);
         var newNode = $([
           '<div mathinput-holder="" show-remove-button="true" class="mathinput-holder">',
-          '<math-input parent-selector=".wiggi-wiz, .config-panel-body" show-code-button="true" fix-backslash="false" editable="true" keypad-auto-open="' + isNew + '" keypad-type="\'basic\'" ng-model="expr" code-model="code" expression="\'' + content + '\'"></math-input>',
+          '<math-input parent-selector=".wiggi-wiz, .config-panel-body" show-code-button="true" fix-backslash="false" editable="true" keypad-auto-open="' + isNew + '" keypad-type="\'basic\'" ng-model="expr" code-model="code" expression-encoded="' + encodedContent + '"></math-input>',
           '</div>'
         ].join(''));
 
@@ -51,7 +54,13 @@ angular.module('corespring-editing.wiggi-wiz-features.mathjax').factory('WiggiMa
       };
 
       this.getMarkUp = function($node, $scope) {
-        var expr = $scope.code || $scope.expr || '';
+        var expr;
+        if ($scope.code) {
+          expr = $scope.code;
+        } else {
+          expr = $scope.expr || '';
+          expr = expr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+        }
         var mathInfo = MathFormatUtils.getMathInfo(expr);
         var res;
         if (mathInfo.mathType === 'MathML') {
