5e503987fcfa9de2dc33fa93192721b404418d42
ed eustace <ed.eustace@gmail.com>
2016-07-26 11:43:36 +0100

    make launch init timeout configurable (#586)

--- modules/container-client-web/app/org/corespring/container/client/V2PlayerConfig.scala
+++ modules/container-client-web/app/org/corespring/container/client/V2PlayerConfig.scala
@@ -1,6 +1,14 @@
 package org.corespring.container.client
 
-case class V2PlayerConfig(rootUrl: Option[String], newRelicRumConfig: Option[NewRelicRumConfig]) {
+/**
+ * @param rootUrl
+ * @param newRelicRumConfig
+ * @param launchTimeout - how long should the launcher wait for the underlying instance to be ready (default: 0 - aka no timeout)
+ */
+case class V2PlayerConfig(
+  rootUrl: Option[String],
+  newRelicRumConfig: Option[NewRelicRumConfig],
+  launchTimeout: Int = 0) {
   val useNewRelic = newRelicRumConfig.isDefined
 }
 
--- modules/container-client-web/test/org/corespring/container/client/controllers/launcher/definitions/ComponentEditorTest.scala
+++ modules/container-client-web/test/org/corespring/container/client/controllers/launcher/definitions/ComponentEditorTest.scala
@@ -3,15 +3,16 @@ package org.corespring.container.client.controllers.launcher.definitions
 import org.corespring.container.client.controllers.launcher.ComponentEditor
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
+import play.api.libs.json.Json._
 
 class ComponentEditorTest extends Specification with Mockito {
 
   trait scope extends Scopes.launcher
   "result" should {
     "call builder.buildJs" in new scope {
-      val editor = ComponentEditor(builder, Map.empty)
+      val editor = new ComponentEditor(0, builder, Map.empty)
       editor.result("url")
-      there was one(builder).buildJs("url", editor.fileNames, editor.options, editor.bootstrap, Map.empty)
+      there was one(builder).buildJs("url", editor.fileNames, editor.options.deepMerge(obj("initTimeout" -> 0)), editor.bootstrap, Map.empty)
     }
   }
 }
